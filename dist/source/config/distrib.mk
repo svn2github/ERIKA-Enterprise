b26513337305bda6754b8bb2f7e1f666d67537bd
nino <nino@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2009-01-22 13:42:14 +0000

    Bugfix: Wrong filename in mcu/microchip_dspic
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@10 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- dist/source/config/distrib.mk
+++ dist/source/config/distrib.mk
@@ -122,7 +122,7 @@ distrib_opt___PIC30__:: \
 		ee_$(DIST)/pkg/mcu/microchip_dspic/inc \
 		ee_$(DIST)/pkg/mcu/microchip_dspic/inc/ee_internal.h \
 		ee_$(DIST)/pkg/mcu/microchip_dspic/src \
-		ee_$(DIST)/pkg/mcu/microchip_dspic/src/ee_myfile.c \
+		ee_$(DIST)/pkg/mcu/microchip_dspic/src/ee_pic30frsh.c \
 		ee_$(DIST)/pkg/board/ee_flex/cfg \
 		ee_$(DIST)/pkg/board/ee_flex/cfg/cfg.mk \
 		ee_$(DIST)/pkg/board/ee_flex/inc \
