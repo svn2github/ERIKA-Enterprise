7e834cf3ad6147f37ee2aa2343b0edc0187b9c5c
nino <nino@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2009-04-02 13:18:50 +0000

    Distribetions restyle.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@61 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- dist/source/config/conf.mk
+++ dist/source/config/conf.mk
@@ -13,13 +13,13 @@
 
 # this variable is used in the "help" rule
 ALL_DISTRIBUTIONS += MPC5
-ALL_DISTRIBUTIONS += NIOS2_ALL NIOS2_TEST NIOS2_FROMBINDIST
-ALL_DISTRIBUTIONS += E7T_ALL E7T_TESTCASE
-ALL_DISTRIBUTIONS += PIC30_ALL PIC30_TESTCASE
-ALL_DISTRIBUTIONS += AVR5_ALL AVR5_TESTCASE
 ALL_DISTRIBUTIONS += COBRA_ICD30_ALL
-ALL_DISTRIBUTIONS += EEFLEX_ALL EECOMMERCIAL_ALL EEBASIC_FP EE
-ALL_DISTRIBUTIONS += EEAVR5_ALL
+ALL_DISTRIBUTIONS += NIOS2_ALL NIOS2_TEST NIOS2_FROMBINDIST
+
+#ALL_DISTRIBUTIONS += E7T_ALL E7T_TESTCASE
+#ALL_DISTRIBUTIONS += PIC30_ALL PIC30_TESTCASE
+#ALL_DISTRIBUTIONS += AVR5_ALL AVR5_TESTCASE
+ALL_DISTRIBUTIONS += EEFLEX EE3ARCH EE3ARCHD
 
 
 # -------------------------------------------------------------------
@@ -57,13 +57,23 @@ NIOS2_FROMBINDIST_THEDIST = __NIOS2__ __OO_BCC1__ __ALLOW_NESTED_IRQ__ __MONO__ 
 # -------------------------------------------------------------------
 # -------------------------------------------------------------------
 
-
-
-
 MPC5 = MPC5_SOURCE
 
 MPC5_SOURCE = __FP__ __MPC566EVB__ __MPC5XX__
 
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+
+# Cobra Automotive Spa
+# source code distribution for the STA2051 Vespucci
+
+COBRA_ICD30_ALL = COBRA_ICD30_ALL_OO
+
+COBRA_ICD30_ALL_OO = __ARM7GNU__ __ST_STA2051__ __COBRA_ICD30__ __OO_BCC1__ __FP__
+
+
+
+# -------------------------------------------------------------------
 # -------------------------------------------------------------------
 
 E7T_ALL = E7T_ALL_OO
@@ -82,25 +92,6 @@ E7T_TESTCASE_OO = __OO_BCC1__ __ASSERT__ __EVALUATOR7T__ __SAMSUNG_KS32C50100__ 
 E7T_FROMBINDIST = E7T_ALL_OO
 
 # -------------------------------------------------------------------
-
-PIC30_EE = PIC30_EE_OO
-
-PIC30_EE_OO = __PIC30__ __OO_BCC1__ __FP__
-
-# -------------------------------------------------------------------
-
-PIC30_TESTCASE = PIC30_TESTCASE_OO
-
-PIC30_TESTCASE_OO = __PIC30__ __OO_BCC1__ __ASSERT__
-
-# -------------------------------------------------------------------
-
-# same as PIC30_ALL!!!
-PIC30_FROMBINDIST = PIC30_ALL_OO
-
-# -------------------------------------------------------------------
-
-
 # -------------------------------------------------------------------
 
 AVR5_EE = AVR5_EE_OO
@@ -118,58 +109,64 @@ AVR5_TESTCASE_OO = __AVR5__ __OO_BCC1__ __ASSERT__
 # same as AVR5_ALL!!!
 AVR5_FROMBINDIST = AVR5_ALL_OO
 
+
+# -------------------------------------------------------------------
 # -------------------------------------------------------------------
 
+PIC30_EE = PIC30_EE_OO
 
-
-
-# Cobra Automotive Spa
-# source code distribution for the STA2051 Vespucci
-
-COBRA_ICD30_ALL = COBRA_ICD30_ALL_OO
-
-COBRA_ICD30_ALL_OO = __ARM7GNU__ __ST_STA2051__ __COBRA_ICD30__ __OO_BCC1__ __FP__
-
+PIC30_EE_OO = __PIC30__ __OO_BCC1__ __FP__
 
 # -------------------------------------------------------------------
 
-# EE Basic, for the distribution on the web site or the commercial distribution
+PIC30_TESTCASE = PIC30_TESTCASE_OO
 
-EEBASIC = EEBASIC_FP
+PIC30_TESTCASE_OO = __PIC30__ __OO_BCC1__ __ASSERT__
 
-EEBASIC_FP = __AVR5__ __PIC30__ __FP__ __EDF__ __AVR5_EXAMPLES__ __PIC30_EXAMPLES__ __ALARMS__ __SEM__
+# -------------------------------------------------------------------
 
+# same as PIC30_ALL!!!
+PIC30_FROMBINDIST = PIC30_ALL_OO
+
+
+# -------------------------------------------------------------------
+# -------------------------------------------------------------------
+
+# EE versions
+
+EE_KERNEL = __FP__ __EDF__ __OO_BCC1__ __ALARMS__ __SEM__
+
+EE_LIBS = __LIB_IEEE802154__
+
+
+EE_PIC30 = __PIC30__ __PIC30_EXAMPLES__
+
+EE_AVR5 = __AVR5__ __AVR5_EXAMPLES__
+
+EE_ARM7 = __ARM7GNU__ __SAMSUNG_KS32C50100__ __EVALUATOR7T__
+
+EE_ARM7_DEV = __ARM7GNU__ __SAMSUNG_KS32C50100__ __EVALUATOR7T__ __unibo_mparm__
+
+
+EEFLEX = EE_KERNEL EE_LIBS EE_PIC30
+
+EE3ARCH = EE_KERNEL EE_LIBS EE_PIC30 EE_AVR5 EE_ARM7
+
+EE3ARCHD = EE_KERNEL EE_LIBS EE_PIC30 EE_AVR5 EE_ARM7_DEV
+
+# -------------------------------------------------------------------
+# OLD, to be removed!
 # -------------------------------------------------------------------
 
 # EE, for the commercial distribution
-
-EE = EE_ALL EE_OPENZB
-
-EE_ALL = __PIC30__ __FP__ __EDF__ __PIC30_EXAMPLES__ __OO_BCC1__  __FRSH__ __SEM_FRSH__ __ALARMS__ __SEM__
-
-EE_OPENZB = __LIB_IEEE802154__
-
+#EE = EE_ALL EE_OPENZB
+#EE_ALL = __PIC30__ __FP__ __EDF__ __PIC30_EXAMPLES__ __OO_BCC1__  __FRSH__ __SEM_FRSH__ __ALARMS__ __SEM__
+#EE_OPENZB = __LIB_IEEE802154__
 #EE_ALL = __AVR5__ __PIC30__ __FP__ __EDF__ __AVR5_EXAMPLES__ __PIC30_EXAMPLES__ __OO_BCC1__  __ALARMS__ __SEM__
 
-
-# -------------------------------------------------------------------
-
 # EE FLEX, for the Flex CDROM
-
-EEFLEX = EEFLEX_ALL
-
-EEFLEX_ALL = __PIC30__ __FP__ __OO_BCC1__ __EDF__ __PIC30_EXAMPLES__ __ALARMS__ __SEM__ __LIB_IEEE802154__
-
-
+#EEFLEX = EEFLEX_ALL
+#EEFLEX_ALL = __PIC30__ __FP__ __OO_BCC1__ __EDF__ __PIC30_EXAMPLES__ __ALARMS__ __SEM__ __LIB_IEEE802154__
 # EE AVR5, by now, for downloading
-EEAVR5 = EEAVR5_ALL
-
-EEAVR5_ALL = __AVR5__ __FP__ __EDF__ __AVR5_EXAMPLES__ __ALARMS__ __SEM__
-
-# -------------------------------------------------------------------
-
-# EE for PIC30, AVR5 and ARM7
-
-EE3ARCH = EE3ARCH_FP
-
-EE3ARCH_FP = __AVR5__ __PIC30__ __ARM7GNU__ __SAMSUNG_KS32C50100__ __EVALUATOR7T__ __unibo_mparm__ __FP__ __EDF__ __OO_BCC1__ __ALARMS__ __SEM__ __AVR5_EXAMPLES__ __PIC30_EXAMPLES__ __LIB_IEEE802154__
+#EEAVR5 = EEAVR5_ALL
+#EEAVR5_ALL = __AVR5__ __FP__ __EDF__ __AVR5_EXAMPLES__ __ALARMS__ __SEM__
