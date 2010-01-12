626f03e2b307faef5561c8a7e156ac5d8fdb544e
nastasi <nastasi@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-01-12 18:46:38 +0000

    Add pic32 files for mrf24j40 hal
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@425 88d5ba13-baa4-4915-ae7a-41304cef7dd7

---
+++ contrib/drivers/radio/mrf24j40/inc/mrf24j40_hal_ee_pic32.h
@@ -0,0 +1,181 @@
+/** 
+* @file mrf24j40_hal_.h
+* @brief MRF24J40 Hw Abstraction Layer using Erika OS
+* @author Gianluca Franchino
+* @author Christian Nastasi
+* @author Mauro Nino Marinoni
+* @date 2009-03-24
+*
+* This file is the definition of the hardware abstraction layer used by all the module
+* of the MRF24J40 library which uses the Erika kernel drivers. 
+*
+* \todo Write something about the hal support.
+*/
+
+// TODO: Check radio pins.
+
+#ifndef __mrf24j40_hal_ee_pic32_h__
+#define __mrf24j40_hal_ee_pic32_h__
+
+#ifndef __USE_SPI__
+#error "MRF24J40 HAL EE : The SPI module from MCU is required!"
+#endif
+#include <mcu/microchip_pic32/inc/ee_spi.h>
+
+#define MRF24J40_SPI_PORT_1	EE_SPI_PORT_1
+#define MRF24J40_SPI_PORT_2	EE_SPI_PORT_2
+
+#ifndef MRF24J40_RESETn
+#define MRF24J40_RESETn		PORTFbits.RF1
+#endif
+
+#ifndef MRF24J40_INT
+#define MRF24J40_INT		PORTAbits.RA14
+#endif
+
+#ifndef MRF24J40_CSn
+#define MRF24J40_CSn		PORTAbits.RA15
+#endif
+
+#ifndef MRF24J40_TRIS_RESETn
+#define MRF24J40_TRIS_RESETn		TRISFbits.TRISF1
+#endif
+
+#ifndef MRF24J40_TRIS_INT
+#define MRF24J40_TRIS_INT		TRISAbits.TRISA14
+#endif
+
+#ifndef MRF24J40_TRIS_CSn
+#define MRF24J40_TRIS_CSn		TRISAbits.TRISA15
+#endif
+
+#ifndef MRF24J40_INTERRUPT_NAME	
+#define MRF24J40_INTERRUPT_NAME		_INT3Interrupt
+#endif
+
+#ifndef MRF24J40_INTERRUPT_FLAG	
+#define MRF24J40_INTERRUPT_FLAG		IFS0bits.INT3IF
+#endif
+
+#ifndef MRF24J40_INTERRUPT_ENABLE
+#define MRF24J40_INTERRUPT_ENABLE 	IEC0bits.INT3IE
+#endif
+
+#ifndef MRF24J40_INTERRUPT_PRIORITY
+#define MRF24J40_INTERRUPT_PRIORITY 	IPC3bits.INT3IP
+#endif
+
+#ifndef MRF24J40_INTERRUPT_EDGE_POLARITY	
+#define MRF24J40_INTERRUPT_EDGE_POLARITY	 INTCONbits.INT3EP
+#endif
+
+//#ifndef MRF24J40_RESETn
+//#define MRF24J40_RESETn		PORTGbits.RG0
+//#endif
+//
+//#ifndef MRF24J40_VREG_EN
+//#define MRF24J40_VREG_EN	PORTGbits.RG12
+//#endif
+//
+//#ifndef MRF24J40_FIFO
+//#ifdef __USE_DEMOBOARD__	/* Demoboard defaults */
+//#define MRF24J40_FIFO		PORTDbits.RD14
+//#else				/* Gianluca's board default*/
+//#define MRF24J40_FIFO		PORTEbits.RE9
+//#endif
+//#endif
+//
+//#ifndef MRF24J40_FIFOP
+//#ifdef __USE_DEMOBOARD__	/* Demoboard defaults */
+//#define MRF24J40_FIFOP		PORTAbits.RA15
+//#else				/* Gianluca's board default*/
+//#define MRF24J40_FIFOP		PORTFbits.RF6
+//#endif
+//#endif
+//
+//#ifndef MRF24J40_CSn
+//#define MRF24J40_CSn		PORTGbits.RG9
+//#endif
+//
+//#ifndef MRF24J40_TRIS_RESETn
+//#define MRF24J40_TRIS_RESETn	TRISGbits.TRISG0
+//#endif
+//
+//#ifndef MRF24J40_TRIS_VREG_EN
+//#define MRF24J40_TRIS_VREG_EN	TRISGbits.TRISG12
+//#endif
+//
+//#ifndef MRF24J40_TRIS_FIFO
+//#ifdef __USE_DEMOBOARD__	/* Demoboard defaults */
+//#define MRF24J40_TRIS_FIFO	TRISDbits.TRISD14
+//#else				/* Gianluca's board default*/
+//#define MRF24J40_TRIS_FIFO	TRISEbits.TRISE9
+//#endif
+//#endif
+//
+//#ifndef MRF24J40_TRIS_FIFOP
+//#ifdef __USE_DEMOBOARD__	/* Demoboard defaults */
+//#define MRF24J40_TRIS_FIFOP	TRISAbits.TRISA15
+//#else				/* Gianluca's board default*/
+//#define MRF24J40_TRIS_FIFOP	TRISFbits.TRISF6
+//#endif
+//#endif
+//
+//#ifndef MRF24J40_TRIS_CSn
+//#define MRF24J40_TRIS_CSn	TRISGbits.TRISG9
+//#endif
+//
+//#ifdef __USE_DEMOBOARD__	/* Demoboard defaults */
+//
+//#ifndef MRF24J40_INTERRUPT_NAME	
+//#define MRF24J40_INTERRUPT_NAME	_INT4Interrupt
+//#endif
+//
+//#ifndef MRF24J40_INTERRUPT_FLAG	
+//#define MRF24J40_INTERRUPT_FLAG	IFS3bits.INT4IF
+//#endif
+//
+//#ifndef MRF24J40_INTERRUPT_ENABLE
+//#define MRF24J40_INTERRUPT_ENABLE IEC3bits.INT4IE
+//#endif
+//
+//#ifndef MRF24J40_INTERRUPT_PRIORITY
+//#define MRF24J40_INTERRUPT_PRIORITY IPC13bits.INT4IP
+//#endif
+//
+//#ifndef MRF24J40_INTERRUPT_EDGE_POLARITY	
+//#define MRF24J40_INTERRUPT_EDGE_POLARITY	 INTCON2bits.INT4EP
+//#endif
+//
+//#else				/* Gianluca's board default*/
+//
+//#ifndef MRF24J40_INTERRUPT_NAME	
+//#define MRF24J40_INTERRUPT_NAME	_INT0Interrupt
+//#endif
+//
+//#ifndef MRF24J40_INTERRUPT_FLAG	
+//#define MRF24J40_INTERRUPT_FLAG	IFS0bits.INT0IF
+//#endif
+//
+//#ifndef MRF24J40_INTERRUPT_ENABLE
+//#define MRF24J40_INTERRUPT_ENABLE IEC0bits.INT0IE
+//#endif
+//
+//#ifndef MRF24J40_INTERRUPT_PRIORITY
+//#define MRF24J40_INTERRUPT_PRIORITY IPC0bits.INT0IP
+//#endif
+//
+//#ifndef MRF24J40_INTERRUPT_EDGE_POLARITY	
+//#define MRF24J40_INTERRUPT_EDGE_POLARITY	 INTCON2bits.INT0EP
+//#endif
+//
+//#endif	/* End default booard selection for ISR */
+
+int8_t	mrf24j40_hal_init(void);
+void	mrf24j40_delay_us(uint16_t delay_count); 
+int8_t	mrf24j40_spi_init(uint8_t port);
+int8_t	mrf24j40_spi_close(void);
+int8_t	mrf24j40_spi_put(uint8_t in, uint8_t *out);
+int8_t	mrf24j40_spi_get(uint8_t *out);
+
+#endif /* Header Protection */
---
+++ contrib/drivers/radio/mrf24j40/src/mrf24j40_hal_ee_pic32.c
@@ -0,0 +1,93 @@
+/** 
+* @file mrf24j40_hal_ee.c
+* @brief MRF24J40 Hw Abstraction Layer using Erika OS
+* @author Gianluca Franchino
+* @author Christian Nastasi
+* @author Mauro Nino Marinoni
+* @date 2009-03-24
+*
+* This file is the  hardware abstraction layer used by all the module
+* of the MRF24J40 library which uses the Erika kernel drivers. 
+*
+* \todo Write something about the hal support.
+*/
+#include <mrf24j40_hal.h>
+
+#ifdef __MICROCHIP_PIC32__
+
+static uint16_t temp_count = 0;
+
+volatile unsigned int pipponia = 0; 
+void mrf24j40_delay_us( uint16_t delay_count ) 
+{
+	temp_count = delay_count +1;
+	//asm volatile("outer: dec _temp_count");	
+	//asm volatile("cp0 _temp_count");
+	//asm volatile("bra z, done");
+	//asm volatile("do #1500, inner" );	
+	//asm volatile("nop");
+	//asm volatile("inner: nop");
+	//asm volatile("bra outer");
+	//asm volatile("done:");
+	EE_UINT32 i;
+	
+	asm volatile(	"lui $v0, %hi(18000)\n\t"
+			"addiu $v0, $v0, %lo(18000)\n\t"
+			"1:\n\t"
+			"addi $v0, $v0, -1\n\t"
+		      	"bne $v0, $zero, 1b\n\t"
+	);
+}
+
+int8_t	mrf24j40_hal_init(void)
+{
+	/* Set the IO pins direction */
+	MRF24J40_TRIS_RESETn = 0;
+	//MRF24J40_TRIS_VREG_EN = 0;
+	MRF24J40_TRIS_INT = 1;
+	//MRF24J40_TRIS_FIFOP = 1;
+	MRF24J40_TRIS_CSn = 0;
+	/* Set interrupt registers */
+	MRF24J40_INTERRUPT_FLAG = 0;
+	MRF24J40_INTERRUPT_PRIORITY = 1;
+	MRF24J40_INTERRUPT_EDGE_POLARITY = 0;
+	MRF24J40_INTERRUPT_ENABLE = 1;
+	return 1;
+}
+
+static uint8_t spi_port; 
+
+int8_t	mrf24j40_spi_init(uint8_t port)
+{
+	spi_port = port;
+	return EE_spi_init(spi_port);
+}
+
+int8_t	mrf24j40_spi_close(void)
+{
+	return EE_spi_close(spi_port);
+}
+
+int8_t	mrf24j40_spi_put(uint8_t in, uint8_t *out)
+{
+	if (out != NULL)
+		return EE_spi_rw_byte(spi_port, in, out);
+	else
+		return EE_spi_write_byte(spi_port, in);
+}
+
+int8_t	mrf24j40_spi_get(uint8_t *out)
+{
+	uint8_t dummy;
+
+	if (out != NULL)
+		return EE_spi_read_byte(spi_port, out);
+	else
+		return EE_spi_read_byte(spi_port, &dummy);
+}
+
+#else
+
+#error "MRF24J40_HAL: Architecture not supported!"
+
+#endif
