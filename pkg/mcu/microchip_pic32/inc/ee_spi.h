dcd3dfc97e71716cb0cf50d4e254647acfdf1697
markintosh <markintosh@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-01-27 14:59:41 +0000

    Fixed some hard-coding
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@449 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- pkg/mcu/microchip_pic32/inc/ee_spi.h
+++ pkg/mcu/microchip_pic32/inc/ee_spi.h
@@ -16,7 +16,10 @@ enum {
 	EE_SPI_PORT_N,
 };
 
+
+#define EE_SPI_NO_ERRORS		1	//Used positive
 #define EE_SPI_ERR_BAD_PORT		1
+
 //#define EE_SPI_ERR_NO_DATA		2
 //#define EE_SPI_ERR_OVERFLOW		3
 //#define EE_SPI_ERR_INT_MODE		4
@@ -37,13 +40,25 @@ enum {
 */
 EE_INT8 EE_spi_init(EE_UINT8 port/*, EE_UINT32 baudrate, EE_UINT16 flags*/); 
 
-__INLINE__ EE_INT8 EE_spi_close(EE_UINT8 port)
-{
-	return 1;
-}
+
 
 /**
-* @brief Send a byte through  the SPI peripheral.
+* @brief Switch-off the SPI peripheral.
+*
+* This function switches-off the SPI peripheral defined by 
+* the input parameter \p port.
+*
+* @param[in] port 	
+* @return	EE_SPI_NO_ERRORS if no errors occurred. Otherwise a negative
+*		number
+*
+* @pre		None
+*/
+EE_INT8 EE_spi_close(EE_UINT8 port); 
+
+
+/**
+* @brief Send a byte through the SPI peripheral.
 *
 * This function send a character (8 bits) through the SPI 
 * port controlling the radio.
@@ -56,6 +71,7 @@ __INLINE__ EE_INT8 EE_spi_close(EE_UINT8 port)
 * @pre		The SPI port must be correctly initialized.
 */
 EE_INT8 EE_spi_rw_byte(EE_UINT8 port, EE_UINT8 data_in, EE_UINT8 *data_out);
+
 
 /**
 * @brief Send a byte through  the SPI peripheral.
@@ -73,6 +89,7 @@ __INLINE__ EE_INT8 EE_spi_write_byte(EE_UINT8 port, EE_UINT8 data)
 {
 	return EE_spi_rw_byte(port, data, &data);
 }
+
 
 /**
 * @brief Get a character through the SPI peripheral.
--- pkg/mcu/microchip_pic32/src/ee_spi.c
+++ pkg/mcu/microchip_pic32/src/ee_spi.c
@@ -8,10 +8,9 @@ __INLINE__ void init_port_1(EE_UINT16 spibrg, EE_UINT16 flags)
 		  _IEC0_SPI1RXIE_MASK;
 	SPI1CON = 0;
 	rData = SPI1BUF; 	/* Clears the receive buffer */	
-	SPI1BRG = 0x1; 	// TODO: change this hard-coding.
-			//Bibo: FCLK_SPI = Fpb/(SPIBRG+1) = 9 Mhz
+	SPI1BRG = spibrg; 	
 	SPI1STATCLR = _SPI1STAT_SPIROV_MASK; 
-	SPI1CON = _SPI1CON_MSTEN_MASK | _SPI1CON_SMP_MASK | _SPI1CON_ON_MASK;
+	SPI1CON = _SPI1CON_MSTEN_MASK | _SPI2CON_CKE_MASK | _SPI1CON_ON_MASK;
 }
 
 __INLINE__ void init_port_2(EE_UINT16 spibrg, EE_UINT16 flags)
@@ -20,44 +19,42 @@ __INLINE__ void init_port_2(EE_UINT16 spibrg, EE_UINT16 flags)
 
 	IEC1CLR = _IEC1_SPI2EIE_MASK | _IEC1_SPI2TXIE_MASK | 
 		  _IEC1_SPI2RXIE_MASK;
-	SPI2CON = 0; 		
-	TRISGCLR = 0x0140;
-	TRISGSET = 0x0800;
-	rData = SPI2BUF; 	/* Clears the receive buffer */
-	SPI2BRG = 0x4; 	// TODO: change this hard-coding.
-			//Bibo: FCLK_SPI = Fpb/(SPIBRG+1) = 9 Mhz
+	SPI2CON = 0;
+	rData = SPI2BUF; 
+	SPI2BRG = spibrg; 	
+			
 	SPI2STATCLR = _SPI2STAT_SPIROV_MASK; 
-	/*@author BibO: trial modification 
-	SPI2CON = _SPI2CON_MSTEN_MASK | _SPI2CON_SMP_MASK |
-		  _SPI2CON_ON_MASK; */
-	SPI2CON = 0x8120;		 
-	/*@author Bibo: end of modification */		
+	
+	SPI2CON = _SPI2CON_MSTEN_MASK |_SPI2CON_ON_MASK |_SPI2CON_CKE_MASK;
+								//0x8120;		 			
 }
 
 EE_INT8 EE_spi_init(EE_UINT8 port/*, EE_UINT32 baudrate, EE_UINT16 flags*/) 
 {
 	if (port == EE_SPI_PORT_1) {
-		init_port_1(0, 0);
-		return 1;
+		init_port_1(0x4, 0); //Bibo: FCLK_SPI = Fpb/(SPIBRG+1) = 8 Mhz
+		return EE_SPI_NO_ERRORS;
 	} else if (port == EE_SPI_PORT_2) {
-		init_port_2(0, 0);
-		return 1;
+		init_port_2(0x4, 0); //Bibo: FCLK_SPI = Fpb/(SPIBRG+1) = 8 Mhz
+		return EE_SPI_NO_ERRORS;
 	}
 	return -EE_SPI_ERR_BAD_PORT;
 }
 
 
-/* chris: TODO: Release something 
+
 EE_INT8 EE_spi_close(EE_UINT8 port)
 {
 	if (port == EE_SPI_PORT_1) {
-		return 1;
+		SPI1CONCLR= _SPI1CON_ON_MASK;
+		return EE_SPI_NO_ERRORS;
 	} else if (port == EE_SPI_PORT_2) {
-		return 1;
+		SPI2CONCLR= _SPI2CON_ON_MASK;
+		return EE_SPI_NO_ERRORS;
 	}
 	return -EE_SPI_ERR_BAD_PORT;
 }
-*/
+
 
 EE_INT8 EE_spi_rw_byte(EE_UINT8 port, EE_UINT8 data_in, EE_UINT8 *data_out)
 {
@@ -72,7 +69,7 @@ EE_INT8 EE_spi_rw_byte(EE_UINT8 port, EE_UINT8 data_in, EE_UINT8 *data_out)
    		while (!SPI1STATbits.SPIRBF);
 		SPI1STATCLR = _SPI1STAT_SPIROV_MASK; 
 		*data_out = SPI1BUF & 0x00FF;
-   		return 1;
+   		return EE_SPI_NO_ERRORS;
 	} else if (port == EE_SPI_PORT_2) {
 		SPI2STATCLR = _SPI2STAT_SPIROV_MASK; 
    		while (!SPI2STATbits.SPITBE); 
@@ -80,7 +77,7 @@ EE_INT8 EE_spi_rw_byte(EE_UINT8 port, EE_UINT8 data_in, EE_UINT8 *data_out)
    		while (!SPI2STATbits.SPIRBF);
 		SPI2STATCLR = _SPI2STAT_SPIROV_MASK; 
 		*data_out = SPI2BUF & 0x00FF;
-   		return 1;
+   		return EE_SPI_NO_ERRORS;
    	}
 	return -EE_SPI_ERR_BAD_PORT;
 }
