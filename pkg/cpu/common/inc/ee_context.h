6f491007530d464efcb231f9857c07d2215b9c5b
dibbe <dibbe@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-03-26 10:47:05 +0000

    Mico32 & Common: added comments on the removal of the "stacked" mark
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@575 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- pkg/cpu/common/inc/ee_context.h
+++ pkg/cpu/common/inc/ee_context.h
@@ -155,8 +155,13 @@ __INLINE__ void __ALWAYS_INLINE__ EE_std_change_context(EE_TID tid)
 /* TID_IS_STACKED_MARK must set the most significative bit */
 #define EE_std_mark_tid_stacked(tid) ((tid) | (EE_TID)TID_IS_STACKED_MARK)
 
-#define EE_std_need_context_change(tid) (((tid) >= 0) || \
-(EE_hal_active_tos != EE_std_thread_tos[tid+1]))
+__INLINE__ int __ALWAYS_INLINE__ EE_std_need_context_change(EE_TID tid)
+{
+    /* FIXME: "tid+1" can be used as an index for arrays even when marked if
+     * EE_TID is defined as an int.  Otherwise, the mark will cause a memory
+     * access violation!  */
+    return (((tid) >= 0) || (EE_hal_active_tos != EE_std_thread_tos[tid+1]));
+}
 
 __INLINE__ void __ALWAYS_INLINE__ EE_hal_stkchange(EE_TID tid)
 {
