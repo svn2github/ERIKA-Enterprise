eafd4db8c1f1f7a187478087f8dcc38b08fc0624
fchecconi <fchecconi@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-05-09 23:45:34 +0000

    Import the last bunch of porting examples.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@621 88d5ba13-baa4-4915-ae7a-41304cef7dd7

---
+++ pkg/cpu/e200z7/inc/ee_debug.h
@@ -0,0 +1,224 @@
+/* ###*B*###
+ * ERIKA Enterprise - a tiny RTOS for small microcontrollers
+ *
+ * Copyright (C) 2002-2010  Evidence Srl
+ *
+ * This file is part of ERIKA Enterprise.
+ *
+ * ERIKA Enterprise is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation, 
+ * (with a special exception described below).
+ *
+ * Linking this code statically or dynamically with other modules is
+ * making a combined work based on this code.  Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this code with independent modules to produce an
+ * executable, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting executable under
+ * terms of your choice, provided that you also meet, for each linked
+ * independent module, the terms and conditions of the license of that
+ * module.  An independent module is a module which is not derived from
+ * or based on this library.  If you modify this code, you may extend
+ * this exception to your version of the code, but you are not
+ * obligated to do so.  If you do not wish to do so, delete this
+ * exception statement from your version.
+ *
+ * ERIKA Enterprise is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with ERIKA Enterprise; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ * ###*E*### */
+
+/*
+ * Some debugging facilities.  The content of this file is strictly connected
+ * with the internals of the kernel and the HAL, so it is likely to change with
+ * new releases of the OS.  It is meant to debug the HAL, the kernel, and to
+ * help in finding some nasty bugs; it is not meant for generale use.
+ * WARNING: DCC support is still incomplete.
+ * Author: 2010 Fabio Checconi
+ */
+
+#ifndef __INCLUDE_E200Z7_DEBUG_H__
+#define __INCLUDE_E200Z7_DEBUG_H__
+
+#include <cpu/e200z7/inc/ee_internal.h>
+
+/*
+ * Stack debugging
+ */
+#ifdef DEBUG_STACK
+
+/*
+ * This value is used as a canary (guard memory location) for stack overflow
+ * detection.  This value has three interesting properties, which tend to cause
+ * an exception if it's read and used by the processor:
+ * 1. It's an invalid instruction
+ * 2. It's a misaligned address
+ * 3. It's an address in the I/O region, not suitable for code, and likely
+ * invalid anyway
+ * The canary is written before and after the actual stack space.  If it is
+ * overwritten, you know that that the stack has been exceeded.
+ */
+#define E200Z7_STACK_CANARY     0xabadbeef
+/*
+ * Value to fill uninitialized memory.  It has the same properties of the
+ * canary.
+ */
+#define E200Z7_FILL_MEMORY      0xce11f00d
+
+#define PRE_STACK_CANARY_LEN	76  /* Enough to invalidate all caller-saved
+                                     * registers */
+#define POST_STACK_CANARY_LEN	4
+
+/* Used as the initaliaziation value of the private stacks in `eccgc.c'.  Write
+ * the canary before and after the actual stack, and fill it with garbage.  C99
+ * syntax and `...' GCC extension are used. */
+#ifdef notyet
+#define E200Z7_STACK_INIT(stack_len) {                                  \
+    [0 ... POST_STACK_CANARY_LEN - 1] = E200Z7_STACK_CANARY,            \
+        [POST_STACK_CANARY_LEN ... stack_len + POST_STACK_CANARY_LEN - 1] \
+        = E200Z7_FILL_MEMORY,                                           \
+        [stack_len + POST_STACK_CANARY_LEN ...                          \
+            stack_len + PRE_STACK_CANARY_LEN + POST_STACK_CANARY_LEN - 1] \
+        = E200Z7_STACK_CANARY                                           \
+        }
+#endif
+#define E200Z7_INIT_TOS_OFFSET (1-POST_STACK_CANARY_LEN)
+#define E200Z7_STACK_DEBUG_LEN (PRE_STACK_CANARY_LEN+POST_STACK_CANARY_LEN)
+
+
+/*
+ * Check that the canary below the stack is intact.  `base_tos' is the initial
+ * value of the top-of-stack.  Return 0 if the canary is ok, and the offset (as
+ * a positive number, measured in bytes) of the first corrupted location
+ * otherwise.
+ */
+__INLINE__ int __ALWAYS_INLINE__ check_pre_stack_canary(EE_ADDR base_tos)
+{
+    int i;
+    int *addr = (int *)base_tos;
+    for (i = 1; i <= PRE_STACK_CANARY_LEN; ++i)
+        if (addr[i] != E200Z7_STACK_CANARY)
+            return i*4;
+    return 0;
+}
+
+
+/*
+ * Check that the canaries before all stacks are intact.  Return 0 if the check
+ * is passed, or a the index of the stack that failed the test.  The first stack
+ * (index 0) is the system stack and has no canary, so it's not checked.  An
+ * underflow of the system stack should cause a bus error anyway.
+ */
+__INLINE__ int __ALWAYS_INLINE__ check_all_pre_stack_canaries(
+    EE_ADDR base_toses[], int num_toses)
+{
+    int i;
+    /* Skip the system stack */
+    for (i = 1; i < num_toses; ++i) {
+        if (0 != check_pre_stack_canary(base_toses[i]))
+            return i;
+    }
+    return 0;
+}
+
+
+#endif /* DEBUG_STACK */
+
+#ifdef __MULTI__
+
+/*
+ * Return the current stack index (according to the HAL).
+ */
+__INLINE__ EE_UREG __ALWAYS_INLINE__ get_current_tos_index()
+{
+    return EE_e200z7_active_tos;
+}
+
+
+/*
+ * Return the index of the stack used for the given task.
+ */
+__INLINE__ EE_UREG __ALWAYS_INLINE__ get_tos_index_for_task(EE_TID tid)
+{
+    return EE_std_thread_tos[tid+1];
+}
+
+asm volatile EE_ADDR get_sp(void)
+{
+! "r3"
+    mr r3, sp
+}
+
+/*
+ * Return the index of the stack more likely to be the current one.  If the
+ * current stack pointer is corrupted, the result is unreliable; it could be
+ * `-1' or any stack index.
+ */
+__INLINE__ EE_UREG __ALWAYS_INLINE__ get_most_likely_tos_index(
+    EE_ADDR base_toses[], int num_toses)
+{
+    EE_ADDR sp; /* Current stack pointer */
+    EE_UREG tosind = (EE_UREG)-1;
+    EE_ADDR tos = (EE_ADDR)-1;
+    int i;
+    sp = get_sp();
+    /* SP is inside the stack whose base top-of-stack is the smallest among
+     * those above SP */
+    for (i = 0; i < num_toses; ++i)
+        if (sp <= base_toses[i] && base_toses[i] < tos) {
+            tosind = i;
+            tos = base_toses[i];
+        }
+    return tosind;
+}
+
+
+/*
+ * Check that all stacks are empty, by comparing the current top-of-stack values
+ * with those given (which should have been obtained through get_base_toses()).
+ * It must be called from main() (when no other task is running).  Return 0 if
+ * the check is passed, or the index of the stack that failed the test.  The
+ * first stack (index 0) should be the current one and it's not checked.
+ */
+__INLINE__ int __ALWAYS_INLINE__ check_toses(
+    EE_ADDR base_toses[], int num_toses)
+{
+    int i;
+    for (i = 1; i < num_toses; ++i)
+        if (base_toses[i] != EE_e200z7_system_tos[i].SYS_tos)
+            return i;
+    return 0;
+}
+
+
+/*
+ * Retrieve the initial top-of-stack value.  Must be called from main() before
+ * any task activation (or StartOS).
+ */
+__INLINE__ int __ALWAYS_INLINE__ get_base_toses(
+    EE_ADDR base_toses[EE_MAX_TASK+1] )
+{
+    int i, max_stack = 0;
+    extern int _estack;     /* Address defined in the linker script */
+    for (i = 0; i < EE_MAX_TASK+1; ++i)
+        if (EE_std_thread_tos[i] > max_stack) {
+            max_stack = EE_std_thread_tos[i];
+            base_toses[max_stack] = EE_e200z7_system_tos[max_stack].SYS_tos;
+        }
+    base_toses[0] = (EE_ADDR)&_estack;
+    return max_stack+1;
+}
+
+#endif /* __MULTI__ */
+
+#endif /* __INCLUDE_E200Z7_DEBUG_H__ */
---
+++ testcase/common/e200z7/test.mk
@@ -0,0 +1,120 @@
+# ###*B*###
+# ERIKA Enterprise - a tiny RTOS for small microcontrollers
+# 
+# Copyright (C) 2002-2008  Evidence Srl
+# 
+# This file is part of ERIKA Enterprise.
+# 
+# ERIKA Enterprise is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation, 
+# (with a special exception described below).
+# 
+# Linking this code statically or dynamically with other modules is
+# making a combined work based on this code.  Thus, the terms and
+# conditions of the GNU General Public License cover the whole
+# combination.
+# 
+# As a special exception, the copyright holders of this library give you
+# permission to link this code with independent modules to produce an
+# executable, regardless of the license terms of these independent
+# modules, and to copy and distribute the resulting executable under
+# terms of your choice, provided that you also meet, for each linked
+# independent module, the terms and conditions of the license of that
+# module.  An independent module is a module which is not derived from
+# or based on this library.  If you modify this code, you may extend
+# this exception to your version of the code, but you are not
+# obligated to do so.  If you do not wish to do so, delete this
+# exception statement from your version.
+# 
+# ERIKA Enterprise is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License version 2 for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# version 2 along with ERIKA Enterprise; if not, write to the
+# Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA.
+# ###*E*###
+
+#
+# Freescale e200z7 testcases
+#
+
+ECLIPSE_HOME ?= /opt/eclipse/
+LAUNCHER_JAR=$(shell ls $(ECLIPSE_HOME)/plugins/org.eclipse.equinox.launcher_*.jar)
+
+#
+# Global scripts
+#
+
+GLOBAL_RTDRUID += \
+	( if test -e tmp/e200z7_rtdruid_partial.xml; then \
+		cat common/rtdruid_common/script_prologue.xml tmp/e200z7_rtdruid_partial.xml common/rtdruid_common/script_epilogue.xml > tmp/build.xml; \
+		cp tmp/build.xml tmp/e200z7_rtdruid_global_build.xml; \
+		cd tmp; java -jar "$(LAUNCHER_JAR)" -application org.eclipse.ant.core.antRunner &>rtdruid_e200z7.log; \
+	fi );
+
+#
+# configurations
+#
+
+# e200z7
+TESTLIST 		+= e200z7
+OUTDIR_COMMANDS_e200z7 	= OUTDIR_COMMANDS_e200z7_source
+CONF_e200z7            	= CONF_e200z7_source
+GLOBAL_CONF 		+=
+DIST_e200z7            	=
+RTDRUID_e200z7		= RTDRUID_e200z7_source
+CLEAN_e200z7           	=
+COMPILE_e200z7         	= COMPILE_e200z7_source
+DEBUG_e200z7           	= DEBUG_e200z7_source
+
+# -------------------------------------------------------------------
+
+TMPDIR = $(EEBASE)/testcase/tmp
+FILE_LOCK = $(TMPDIR)/e200z7_manual.lock
+RTDRUID_GENERATE_LOCK = $(TMPDIR)/rtdruid_generate_lock.lock
+LOCKFILE= lockfile -1 -r-1
+DIST_LOCK = $(TMPDIR)/dist.lock
+
+# -------------------------------------------------------------------
+
+OUTDIR_COMMANDS_e200z7_source = \
+	( cd $@; cp -sf ../*.* .; \
+	);
+
+# -------------------------------------------------------------------
+# # These are the commands used by e200z7_dist_src
+
+# this simply parses the OIL file and then raises a flag if there is need to generate a source distribution
+CONF_e200z7_source = \
+	@echo TMPDIR=$(TMPDIR) \
+	@echo CONF $$(OUTDIR_PREFIX)$(1); \
+	cat $$(OUTDIR_PREFIX)$(1)/appl.oil | gcc -c - -E -P -I$$(EEBASE)/pkg $$(addprefix -D, $$(shell $$(DEMUX2) $(1))) -D$$(thearch) -o - >$$(OUTDIR_PREFIX)$(1)/ee.oil; \
+	touch $$(TMPDIR)/e200z7_dist_src_buildsourcedistribution.flg;
+
+# if the flag has been raised, generate the source distribution
+GLOBAL_CONF_e200z7_source = \
+	( if test -e tmp/e200z7_dist_src_buildsourcedistribution.flg; then \
+		make -C $${EEBASE}/dist/source DIST=e200z7_TESTCASE e200z7_MOVE=Y &>tmp/e200z7_dist_src_buildsourcedistribution.log; \
+	fi );
+
+# Generate the rt-druid files...
+RTDRUID_e200z7_source = \
+	@echo RTDRUID $$(OUTDIR_PREFIX)$(1); \
+	echo \<rtdruid.Oil.Configurator inputfile=\"$$(OUTDIR_PREFIX)$(1)/ee.oil\" outputdir=\"$$(OUTDIR_PREFIX)$(1)/Debug\" /\> >> $$(TMPDIR)/e200z7_rtdruid_partial.xml;
+
+# take also a look to GLOBAL_RTDRUID at the top of the file!!!
+
+COMPILE_e200z7_source = +if $$(MAKE) $(PARAMETERS) NODEPS=1 -C $$(OUTDIR_PREFIX)$(1)/Debug &>$$(OUTDIR_PREFIX)$(1)/compile.log; then echo OK $$(EXPERIMENT) $$(OUTDIR_PREFIX)$(1) >>$$(TMPDIR)/ok.log; else echo ERROR $$(EXPERIMENT) $$(OUTDIR_PREFIX)$(1) >>$$(TMPDIR)/errors.log; fi
+
+DEBUG_e200z7_source = \
+	cp e200z7/testcase.cmd $$(OUTDIR_PREFIX)$(1); \
+	$$(LOCKFILE) $$(FILE_LOCK); \
+		echo "                                     " >> $$(TMPDIR)/e200z7_jobs.cmd; \
+		echo "cd $$(OUTDIR_PREFIX)$(1)" >> $$(TMPDIR)/e200z7_jobs.cmd; \
+		echo "cf testcase.cmd                      " >> $$(TMPDIR)/e200z7_jobs.cmd; \
+		cp -u e200z7/e200z7.cmd $$(TMPDIR)/e200z7.cmd; \
+	rm -f $$(FILE_LOCK); \
