8ed19a84c3f9b5598a3cac95b8e16b9e31b79ea1
markintosh <markintosh@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-05-21 15:05:51 +0000

    Introduced the functions for calculating the system and the peripheral clock
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@646 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- pkg/cpu/pic32/inc/ee_utils.h
+++ pkg/cpu/pic32/inc/ee_utils.h
@@ -4,6 +4,24 @@
 #include "cpu/pic32/inc/ee_compiler.h"
 #include "ee_pic32regs.h"
 
+
+/* External Clock Source, redefine in the application if differs from this
+*  value */
+#ifndef CLOCK_SOURCE
+#define CLOCK_SOURCE 8000000ul
+#endif 
+
+
+#ifndef DEVCFG2
+#define DEVCFG2  0xBFC02FF4 //virtual address of the register
+#endif //DEVCFG2
+
+#ifndef PLLIDIV_MASK
+#define PLLIDIV_MASK 0x7
+#endif 
+
+
+/* Function used to calculate the initialize the system */
 __INLINE__ void __ALWAYS_INLINE__ EE_system_init(void)
 {
 	EE_UREG r;
@@ -15,6 +33,40 @@ __INLINE__ void __ALWAYS_INLINE__ EE_system_init(void)
         asm volatile("mtc0   %0,$13" : "+r"(r));
         INTCONSET = _INTCON_MVEC_MASK;
 	ASM_EN_INT; 		// Enable IRQ
+}
+
+/* Function used to calculate the system clock */
+__INLINE__ unsigned int __ALWAYS_INLINE__ EE_get_system_clock(void)
+{
+	unsigned int FPPLIDIV_VAL [8] = {1,2,3,4,5,6,10,12};
+	unsigned int PLLODIV_VAL [8] = {1,2,4,8,16,32,64,256};
+	unsigned int PLLMULT_VAL [8] = {15,16,17,18,19,20,21,24};
+
+	/* Extraction of pplodiv */
+	unsigned int pllodiv =  (unsigned int) OSCCONbits.PLLODIV;
+	pllodiv = PLLODIV_VAL[pllodiv];	
+	
+	/* Extraction of pplmult */
+	unsigned int pllmult =  (unsigned int) OSCCONbits.PLLMULT;
+	pllmult = PLLMULT_VAL[pllmult];
+	
+	/* Extraction of pplidiv */
+	unsigned int *app;
+	app = (void *) DEVCFG2;
+	unsigned int fpllidiv = *app;	
+	fpllidiv =  FPPLIDIV_VAL[(fpllidiv & PLLIDIV_MASK)];
+	  
+	return  (CLOCK_SOURCE * pllmult / (fpllidiv * pllodiv));
+}
+
+
+/* Function used to calculate the peripherals clock */
+__INLINE__ unsigned int __ALWAYS_INLINE__ EE_get_peripheral_clock(void)
+{
+	unsigned int PBDIV_VAL [4] = {1,2,4,8};
+	unsigned int pbdiv = PBDIV_VAL[OSCCONbits.PBDIV];
+
+	return  (EE_get_system_clock() / pbdiv);
 }
 
 #endif  /* __INCLUDE_PIC32_EE_UTILS_H__ */
