cc9667f515573578344e788fbf18d510d1354a08
dario <dario@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-06-28 13:02:59 +0000

    mico32: proper license in modified lwip files
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@703 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- contrib/lwip/lwip_1_3_2/src/core/udp.c
+++ contrib/lwip/lwip_1_3_2/src/core/udp.c
@@ -8,6 +8,9 @@
  * Copyright (c) 2001-2004 Swedish Institute of Computer Science.
  * All rights reserved.
  *
+ * ERIKA Enterprise - a tiny RTOS for small microcontrollers
+ * Copyright (C) 2010  Evidence Srl
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
@@ -33,6 +36,7 @@
  * This file is part of the lwIP TCP/IP stack.
  *
  * Author: Adam Dunkels <adam@sics.se>
+ * Author: 2010,  Dario Di Stefano (Added time analysis).
  *
  */
 
@@ -44,52 +48,6 @@
  */
 
 /* @todo Check the use of '(struct udp_pcb).chksum_len_rx'!
- */
- 
- /* ###*B*###
- * ERIKA Enterprise - a tiny RTOS for small microcontrollers
- *
- * Copyright (C) 2002-2010  Evidence Srl
- *
- * This file is part of ERIKA Enterprise.
- *
- * ERIKA Enterprise is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation,
- * (with a special exception described below).
- *
- * Linking this code statically or dynamically with other modules is
- * making a combined work based on this code.  Thus, the terms and
- * conditions of the GNU General Public License cover the whole
- * combination.
- *
- * As a special exception, the copyright holders of this library give you
- * permission to link this code with independent modules to produce an
- * executable, regardless of the license terms of these independent
- * modules, and to copy and distribute the resulting executable under
- * terms of your choice, provided that you also meet, for each linked
- * independent module, the terms and conditions of the license of that
- * module.  An independent module is a module which is not derived from
- * or based on this library.  If you modify this code, you may extend
- * this exception to your version of the code, but you are not
- * obligated to do so.  If you do not wish to do so, delete this
- * exception statement from your version.
- *
- * ERIKA Enterprise is distributed in the hope that it will be
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License version 2 for more details.
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with ERIKA Enterprise; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA.
- * ###*E*### */
-
-/*
- * UDP protocol.
- * Author: 2010,  Dario Di Stefano.
- * Note: Added time analysis.
  */
 
 #include "lwip/opt.h"
