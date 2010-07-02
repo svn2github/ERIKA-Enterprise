ebd6097ec6fb3301909be18d85b6093b2d048096
dibbe <dibbe@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-07-02 01:44:40 +0000

    Console: use Erika types when available
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@712 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- contrib/console/inc/hal/compiler.h
+++ contrib/console/inc/hal/compiler.h
@@ -10,6 +10,42 @@
 /** 
 * @name Basic type representations.
 * @{ */
+#ifdef __ERIKA__
+
+#include <ee.h>
+
+#ifndef UINT8_T
+#define UINT8_T
+typedef EE_UINT8 uint8_t;	/**< Unsigned 8 bit intgerer. */
+#endif
+
+#ifndef UINT16_T
+#define UINT16_T
+typedef EE_UINT16 uint16_t;	/**< Unsigned 16 bit intgerer. */
+#endif
+
+#ifndef UINT32_T
+#define UINT32_T
+typedef EE_UINT32 uint32_t;	/**< Unsigned 32 bit intgerer. */
+#endif
+
+#ifndef INT8_T
+#define INT8_T
+typedef EE_INT8 int8_t;		/**< Signed 8 bit intgerer. */
+#endif
+
+#ifndef INT16_T
+#define INT16_T
+typedef EE_INT16 int16_t;	/**< Signed 16 bit intgerer. */
+#endif
+
+#ifndef INT32_T
+#define INT32_T
+typedef EE_INT32 int32_t;	/**< Signed 32 bit intgerer. */
+#endif
+
+#else /* __ERIKA__ */
+
 #ifndef UINT8_T
 #define UINT8_T
 typedef unsigned char uint8_t;		/**< Unsigned 8 bit intgerer. */
@@ -27,19 +63,20 @@ typedef unsigned long int uint32_t;	/**< Unsigned 32 bit intgerer. */
 
 #ifndef INT8_T
 #define INT8_T
-typedef signed char int8_t;		/**< Unsigned 8 bit intgerer. */
+typedef signed char int8_t;		/**< Signed 8 bit intgerer. */
 #endif
 
 #ifndef INT16_T
 #define INT16_T
-typedef signed short int int16_t;	/**< Unsigned 16 bit intgerer. */
+typedef signed short int int16_t;	/**< Signed 16 bit intgerer. */
 #endif
 
 #ifndef INT32_T
 #define INT32_T
-typedef signed long int int32_t;	/**< Unsigned 32 bit intgerer. */
+typedef signed long int int32_t;	/**< Signed 32 bit intgerer. */
 #endif
 
+#endif /* __ERIKA__ */
 /**  @} */
 
 /** \brief Compiler inlining keywords. */
