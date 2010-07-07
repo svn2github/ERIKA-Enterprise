ecdf2e3d12b25777a0298947aaf54eb4316e99df
fprosperi <fprosperi@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-07-07 17:25:36 +0000

    Added first double channel touch driver version.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@720 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- contrib/drivers/touch/cfg/cfg.mk
+++ contrib/drivers/touch/cfg/cfg.mk
@@ -6,8 +6,11 @@ ifeq ($(findstring __LIB_TOUCH__,$(LIB_OPT)),__LIB_TOUCH__)
 ## Application files
 ##
 
-EE_SRCS += contrib/drivers/touch/src/touch.c
-EE_SRCS += contrib/drivers/touch/src/touch_timer_dspic30.c
-EE_SRCS += contrib/drivers/touch/src/touch_kal_erika.c
+EE_SRCS += contrib/drivers/touch/src/touch_1.c
+EE_SRCS += contrib/drivers/touch/src/touch_1_timer_dspic30.c
+EE_SRCS += contrib/drivers/touch/src/touch_1_kal_erika.c
+EE_SRCS += contrib/drivers/touch/src/touch_2.c
+EE_SRCS += contrib/drivers/touch/src/touch_2_timer_dspic30.c
+EE_SRCS += contrib/drivers/touch/src/touch_2_kal_erika.c
 
 endif
--- contrib/drivers/touch/inc/touch.h
+++ contrib/drivers/touch/inc/touch.h
@@ -4,9 +4,16 @@
 #define __FOUR_WIRES__
 #define __LOW_LEVEL_MEASUREMENT__
 
-#include<touch_compiler.h>
-#include<touch_hal.h>
-#include<touch_kal.h>
+#include "touch_compiler.h"
+#include "touch_hal.h"
+
+#ifdef __USE_TOUCH__
+#include "touch_1_kal.h"
+#endif // __USE_TOUCH__
+
+#ifdef __USE_TOUCH_2__
+#include "touch_2_kal.h"
+#endif // __USE_TOUCH_2__
 
 #define TOUCH_X_AXIS	0
 #define TOUCH_Y_AXIS	1
@@ -15,14 +22,14 @@
 #define SAMPLES_FOR_ONE_TRUE_XY_PAIR    5             // Number of Measurements for one true X/Y-Coordinate
 #define END_OF_ARRAY                    SAMPLES_FOR_ONE_TRUE_XY_PAIR-1
 #define MEDIAN                          (SAMPLES_FOR_ONE_TRUE_XY_PAIR-1)/2      // odd-numbered: SAMPLES_FOR_ONE_TRUE_XY_PAIR
-#define MAXIMUM_LOW_LEVEL		1000          // Invalid Measurement for Low_Level > MAXIMUM_LOW_LEVEL
+#define MAXIMUM_LOW_LEVEL			1000          // Invalid Measurement for Low_Level > MAXIMUM_LOW_LEVEL
 #define RTOUCH_MAXIMUM_LEVEL		4000
 #define MAXIMUM_UNTOUCH_CONDITIONS	100           // Number of (successive) Invalid Measurements till Untouch Condition
-#define SLEEP_COUNTDOWN			2000
+#define SLEEP_COUNTDOWN				2000
 #define TOUCH_ACTIVATION_TIME		2000
 
 #define TOUCH_ERROR_CONFIGURED		0
-#define TOUCH_ERROR_NONE		0
+#define TOUCH_ERROR_NONE			0
 #define TOUCH_ERROR_WRONG_AXIS		1
 #define TOUCH_ERROR_RAW_NOT_READY 	2
 #define TOUCH_ERROR_NOT_TUNED		3
@@ -45,11 +52,11 @@ typedef struct {
 
 typedef struct {
 	uint16_t xt1;
-	uint16_t yt1; 
+	uint16_t yt1;
 	uint16_t xt2;
 	uint16_t yt2;
 	uint16_t xt3;
-	uint16_t yt3;	
+	uint16_t yt3;
 } tune_raw_t;
 
 typedef struct {
@@ -64,30 +71,49 @@ typedef struct {
 /****************************************************************************
   Function definitions
 ****************************************************************************/
-int8_t touch_poll_raw_position(uint8_t, int16_t *);
-int8_t touch_poll_u_position(uint8_t, int16_t *);
-int8_t touch_poll_s_position(uint8_t, int16_t *);
-int8_t touch_wait_raw_position(uint8_t, int16_t *);
-int8_t touch_wait_u_position(uint8_t, int16_t *);
-int8_t touch_wait_s_position(uint8_t, int16_t *);
 
-int8_t	touch_set_dimension(uint8_t, uint16_t);
+#ifdef __USE_TOUCH__
+int8_t touch_1_poll_raw_position(uint8_t, int16_t *);
+int8_t touch_1_poll_u_position(uint8_t, int16_t *);
+int8_t touch_1_poll_s_position(uint8_t, int16_t *);
+int8_t touch_1_wait_raw_position(uint8_t, int16_t *);
+int8_t touch_1_wait_u_position(uint8_t, int16_t *);
+int8_t touch_1_wait_s_position(uint8_t, int16_t *);
+int8_t touch_1_set_dimension(uint8_t, uint16_t);
+void touch_1_raw_init(void);
+void touch_1_tune(tune_raw_t *);
 
-void touch_raw_init();
-void touch_tune(tune_raw_t *);
-
-void touch_calibrate(tune_raw_t *);
-void touch_set_ADC_parameters();
-void touch_set_activation_time(uint16_t);
-
-COMPILER_INLINE void touch_start(void)
+COMPILER_INLINE void touch_1_start(void)
 {
-	touch_timer_start();
+	touch_1_timer_start();
 }
 
-COMPILER_INLINE void touch_stop(void)
+COMPILER_INLINE void touch_1_stop(void)
 {
-	touch_timer_stop();
+	touch_1_timer_stop();
 }
+#endif // __USE_TOUCH__
+
+#ifdef __USE_TOUCH_2__
+int8_t touch_2_poll_raw_position(uint8_t, int16_t *);
+int8_t touch_2_poll_u_position(uint8_t, int16_t *);
+int8_t touch_2_poll_s_position(uint8_t, int16_t *);
+int8_t touch_2_wait_raw_position(uint8_t, int16_t *);
+int8_t touch_2_wait_u_position(uint8_t, int16_t *);
+int8_t touch_2_wait_s_position(uint8_t, int16_t *);
+int8_t touch_2_set_dimension(uint8_t, uint16_t);
+void touch_2_raw_init(void);
+void touch_2_tune(tune_raw_t *);
+
+COMPILER_INLINE void touch_2_start(void)
+{
+	touch_2_timer_start();
+}
+
+COMPILER_INLINE void touch_2_stop(void)
+{
+	touch_2_timer_stop();
+}
+#endif // __USE_TOUCH_2__
 
 #endif // __TOUCH_H__
