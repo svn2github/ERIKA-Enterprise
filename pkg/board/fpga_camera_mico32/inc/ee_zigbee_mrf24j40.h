96d88759ad34fa206e01a92692875e76fbbdf168
dario <dario@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-07-19 09:03:41 +0000

    mico32: mrf24j40 xp2 board header file updated
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@745 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- pkg/board/fpga_camera_mico32/inc/ee_zigbee_mrf24j40.h
+++ pkg/board/fpga_camera_mico32/inc/ee_zigbee_mrf24j40.h
@@ -21,6 +21,8 @@
 //  LOCATE COMP "misc_gpioPIO_IO_1" SITE "77" ;     -> INT2 	(fpga input)
 //  LOCATE COMP "misc_gpioPIO_IO_2" SITE "82" ;     -> FIFOP 	(fpga input)
 //  LOCATE COMP "misc_gpioPIO_IO_3" SITE "76" ;     -> CN1 		(fpga input)
+//  LOCATE COMP "misc_gpioPIO_IO_0" SITE "75" ;     -> ~INT1  	(fpga input)
+//  LOCATE COMP "misc_gpioPIO_IO_1" SITE "77" ;     -> ~INT2	(fpga input)
 //  LOCATE COMP "misc_gpioPIO_IO_4" SITE "122";     -> DL3 		(fpga output)
 //  LOCATE COMP "misc_gpioPIO_IO_5" SITE "86" ;     -> GP1 		(fpga output)
 //  LOCATE COMP "misc_gpioPIO_IO_6" SITE "84" ;     -> CCA 		(fpga output)
@@ -28,18 +30,30 @@
 #define EE_INT1_BIT 			(0) 
 #define EE_INT2_BIT 			(1)
 #define EE_FIFOP_BIT 			(2) 
-#define EE_CN1_BIT 				(3) 
+#define EE_CN1_BIT 				(3)
+#define EE_NOT_INT1_BIT 		(4)
+#define EE_NOT_INT2_BIT			(5)
 #define EE_DL3_BIT 				(0) 
 #define EE_GP1_BIT 				(1)
 #define EE_CCA_BIT 				(2) 
 
-#define EE_INT1_MASK 			(0x01) 
-#define EE_INT2_MASK 			(0x02)
-#define EE_FIFOP_MASK 			(0x04)
-#define EE_CN1_MASK				(0x08)
-#define EE_DL3_MASK				(0x01) 
-#define EE_GP1_MASK				(0x02) 
-#define EE_CCA_MASK				(0x04) 
+#define EE_INT1_MASK 			(0x0001) 
+#define EE_INT2_MASK 			(0x0002)
+#define EE_FIFOP_MASK 			(0x0004)
+#define EE_CN1_MASK				(0x0008)
+#define EE_NOT_INT1_MASK 		(0x0010) 
+#define EE_NOT_INT2_MASK 		(0x0020)
+#define EE_DL3_MASK				(0x0001) 
+#define EE_GP1_MASK				(0x0002) 
+#define EE_CCA_MASK				(0x0004)
+
+#ifdef INT_POLARITY_HIGH
+#define EE_INTPIN_MASK 			EE_INT2_MASK
+#define EE_INTPIN_BIT 			EE_INT2_BIT
+#else
+#define EE_INTPIN_MASK 			EE_NOT_INT2_MASK
+#define EE_INTPIN_BIT 			EE_NOT_INT2_BIT
+#endif 
 
 #define EE_MRF24J40_BOARD_FUNCTION(uc,lc) \
 __INLINE__ void __ALWAYS_INLINE__ EE_mrf24j40_gpio_IRQ_pre_stub(void){ \
@@ -47,7 +61,7 @@ __INLINE__ void __ALWAYS_INLINE__ EE_mrf24j40_gpio_IRQ_pre_stub(void){ \
 } \
 __INLINE__ void __ALWAYS_INLINE__ EE_mrf24j40_gpio_IRQ_post_stub(void){ \
 	while(1){ \
-		if( (EE_misc_gpio_read_data() & EE_INT2_MASK) == 0 ) \
+		if( (EE_misc_gpio_read_data() & EE_INTPIN_MASK) == 0 ) \
 		break; \
 	} \
 } \
@@ -64,19 +78,17 @@ __INLINE__ void __ALWAYS_INLINE__ EE_mrf24j40_gpio_write_output_pins(int val){ \
 	cat3(EE_, lc, _write_data)(val); \
 } \
 __INLINE__ void __ALWAYS_INLINE__ EE_mrf24j40_gpio_enable_IRQ(void){ \
-	cat3(EE_, lc, _write_bit_irqMask)(1, EE_INT2_BIT); \
+	cat3(EE_, lc, _write_bit_irqMask)(1, EE_INTPIN_BIT); \
 } \
 __INLINE__ void __ALWAYS_INLINE__ EE_mrf24j40_gpio_disable_IRQ(void){ \
-	cat3(EE_, lc, _write_bit_irqMask)(0, EE_INT2_BIT); \
+	cat3(EE_, lc, _write_bit_irqMask)(0, EE_INTPIN_BIT); \
 } \
 __INLINE__ int __ALWAYS_INLINE__ EE_mrf24j40_gpio_IRQ_enabled(void){ \
-	return (cat3(EE_, lc, _read_irqMask)() & EE_INT2_MASK); \
+	return (cat3(EE_, lc, _read_irqMask)() & EE_INTPIN_MASK); \
 }
 
 EE_MRF24J40_BOARD_FUNCTION(EE_MRF24J40_GPIO_NAME_UC, EE_MRF24J40_GPIO_NAME_LC)
 
-//#define EE_mrf24j40_int_enable()			EE_misc_gpio_write_bit_irqMask(1, EE_INT2_BIT)
-//#define EE_mrf24j40_int_disable()			EE_misc_gpio_write_bit_irqMask(0, EE_INT2_BIT)
 __INLINE__ void __ALWAYS_INLINE__ EE_mrf24j40_gpio_wake_active(void){
 	EE_mrf24j40_gpio_wake_pin(1);
 }
