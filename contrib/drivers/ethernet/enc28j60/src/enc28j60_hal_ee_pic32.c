6ffedc54a6b248b080ff99206d8ad9035a441251
andreaazzara <andreaazzara@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-09-06 13:15:44 +0000

    fixed inclusion in pic32 hal
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@806 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- contrib/drivers/ethernet/enc28j60/src/enc28j60_hal_ee_pic32.c
+++ contrib/drivers/ethernet/enc28j60/src/enc28j60_hal_ee_pic32.c
@@ -7,7 +7,8 @@
 
 #include "enc28j60.h"
 #include "cpu/pic32/inc/ee_utils.h"
-
+#include "enc28j60_debug.h"
+#include "enc28j60_time_debug.h"
 
 /* ---------------------- Global variables --------------------------------- */
 EE_enc28j60_st ee_enc28j60_st = {
