ced714735253090ab3276faa14d8daada15b3a00
dario <dario@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-09-28 17:04:30 +0000

    scicoslab: encoder block function updated
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@836 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- contrib/scicos/src/pic30/flex_daughter_encoder.c
+++ contrib/scicos/src/pic30/flex_daughter_encoder.c
@@ -28,38 +28,9 @@
 
 #define EE_scicos_encoder_get_enc_id() block->ipar[0]
 //#define EE_scicos_encoder_get_gain() block->rpar[0]
-
-#define EE_ENCODER_HW 		0
-#define EE_ENCODER_SW 		1
-#define Deg_2_Rad			1.745329251994330e-002F
-#define EE_ENCODER_HW_GAIN	Deg_2_Rad
-#define EE_ENCODER_SW_GAIN	Deg_2_Rad*2	// TODO: check this value...
-
-#ifdef __USE_DEMOBOARD__
-#define EE_ENCODER_SW_PINA	PORTDbits.RD7 	// CN16
-#define EE_ENCODER_SW_PINB	PORTCbits.RC4	// digital input
-#define EE_ENCODER_SW_ISR()	ISR2(_CNInterrupt) //interrupt handler
-#define EE_encoder_SW_clear_ISR_flag() IFS1bits.CNIF = 0
-#else
-#ifdef __USE_MOTIONBOARD__
-#define EE_ENCODER_SW_PINA	PORTDbits.RD8 	// IC1
-#define EE_ENCODER_SW_PINB	PORTBbits.RB11	// digital input
-#define EE_ENCODER_SW_ISR()	ISR2(_IC1Interrupt)	//interrupt handler
-#define EE_encoder_SW_clear_ISR_flag() IFS0bits.IC1IF = 0
-#endif
-#endif
-
-int ee_encsw_poscnts = 0;
-
-static float EE_encoder_SW_get_position(float sw_gain)
-{
-	return (float)EE_encoder_SW_get_ticks() * sw_gain;
-}
-
-static float EE_encoder_get_position(float hw_gain)
-{
-	return (float)EE_encoder_get_ticks() * hw_gain;
-}
+//#define Deg_2_Rad			1.745329251994330e-002F
+//#define EE_ENCODER_HW_GAIN	Deg_2_Rad
+//#define EE_ENCODER_SW_GAIN	Deg_2_Rad*2.0                           // 2.0 
 
 static void init(scicos_block *block)
 {
@@ -72,38 +43,46 @@ static void init(scicos_block *block)
 		* (QEI) module and associated operational modes.
 		* The QEI module provides the interface to incremental
 		* encoders for obtaining mechanical position data.
+		* Max Resolution: 4x.
 		*/
-		EE_encoder_init(1, 1); //** encoder initialization 
+		EE_encoder_init(1, 1); 
 	}
-	else // id==2
+	else // sw encoder
 	{
 		/**
 		* The second encoder uses the SW encoder driver library
 		* and associated operational modes.
 		* The driver functioning is based on interrupts on every edge 
-		* to update with the max resolution.
+		* of channel A so we haven't the max resolution, but only 2x.
 		*/
-		EE_encoder_SW_init();
+		EE_encoder_SW_init(1, 1);
 	}
 }
 
 static void inout(scicos_block *block)
 {
-	float * y = block->outptr[0];
+	float *y = block->outptr[0];
 	int id = EE_scicos_encoder_get_enc_id();
 	
 	if(id == EE_ENCODER_HW)
 	{
-		//** the encoder return a float value coded in signed pulse units
-		//** this means that the dspic register MUST be read as "int" 
-		//** in order to keep the signed comp. 2' bynary rapresentation.
-		//** the scaling to degree/radian/centesimal/mm will be done 
-		//** in a later release.
-		y[0] = (float) EE_encoder_get_position(EE_ENCODER_HW_GAIN);
+		/** 
+		* The encoder return a float value coded in signed pulse units
+		* this means that the dspic register MUST be read as "int" 
+		* in order to keep the signed comp. 2' bynary rapresentation.
+		* the scaling to degree/radian/centesimal/mm must be done 
+		* in the scicos diagram.
+		*/
+		y[0] = (float) EE_encoder_get_ticks();
 	}
-	else // id==2
+	else // sw encoder
 	{
-		y[0] = (float) EE_encoder_SW_get_position(EE_ENCODER_SW_GAIN);
+		/** 
+		* Because of 2x resolution, we have to double the number of counter ticks
+		* to have the same behaviour of hw encoder. 
+		* TODO: 4x resolution in the next releases...
+		*/
+		y[0] = (float) EE_encoder_SW_get_ticks() * 2.0;
 	}
 }
 
@@ -118,7 +97,7 @@ void flex_daughter_encoder(scicos_block *block,int flag)
 		inout(block);
 		break;
 
-	case StateUpdate: /* get input */
+	case StateUpdate:  /* get input */
 		break;
 
 	case Initialization:  /* initialisation */
@@ -131,23 +110,3 @@ void flex_daughter_encoder(scicos_block *block,int flag)
 	}
 }
 
-EE_ENCODER_SW_ISR()
-{
-	EE_encoder_SW_clear_ISR_flag();
-	if(EE_ENCODER_SW_PINA)			// if 1, means rising edge
-	{
-		if(EE_ENCODER_SW_PINB)		// reverse - PhaseA lags PhaseB
-			ee_encsw_poscnts--;
-		else						// forward - PhaseA leads PhaseB
-			ee_encsw_poscnts++;
-		
-	}
-	else							// if 0, means falling edge
-	{
-		if(EE_ENCODER_SW_PINB)		// forward - PhaseA leads PhaseB
-			ee_encsw_poscnts++;
-		else						// reverse - PhaseA lags PhaseB
-			ee_encsw_poscnts--;
-		
-	}
-}
