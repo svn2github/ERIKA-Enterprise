d3a03ea7d582d617d3b8711d871ccc6922078ac4
markintosh <markintosh@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-10-12 10:51:49 +0000

    IPERMOB Mother Board added the support for an extension board with camera and leds
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@861 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- pkg/board/ipermob_mb_pic32/inc/ee_board.h
+++ pkg/board/ipermob_mb_pic32/inc/ee_board.h
@@ -11,7 +11,7 @@
 
 
 
-#ifdef __USE_LEDS__
+#if (defined __USE_LEDS__) && (!defined __SENSYS_BOARD__) 
 __INLINE__ void __ALWAYS_INLINE__ EE_leds_init(void)
 {
 	TRISECLR = 0x0F;
@@ -60,6 +60,48 @@ __INLINE__ void __ALWAYS_INLINE__ EE_leds_toggle(void)
 	EE_led_1_toggle;
 	EE_led_2_toggle;
 	EE_led_3_toggle;
+}
+
+#elif (defined __USE_LEDS__) && (defined __SENSYS_BOARD__) 
+
+__INLINE__ void __ALWAYS_INLINE__ EE_leds_init(void)
+{
+	TRISBCLR = 0x300;
+	LATBCLR = 0X300;
+}
+
+#define SB_PIC32_CAT3(x,y,z) x ## y ## z
+
+#define EE_led_0_on	(LATBSET = 0x100)
+#define EE_led_1_on	(LATBSET = 0x200)
+#define EE_led_on(n)	SB_PIC32_CAT3(EE_led_,n,_on)
+
+#define EE_led_0_off	(LATBCLR = 0x100)
+#define EE_led_1_off	(LATBCLR = 0x200)
+
+#define EE_led_off(n)	SB_PIC32_CAT3(EE_led_,n,_off)
+
+#define EE_led_0_toggle		(LATBINV = 0x100)
+#define EE_led_1_toggle		(LATBINV = 0x200)
+#define EE_led_toggle(n)	SB_PIC32_CAT3(EE_led_,n,_toggle)
+
+__INLINE__ void __ALWAYS_INLINE__ EE_leds_on(void) 
+{
+	EE_led_0_on;
+	EE_led_1_on;
+}
+
+__INLINE__ void __ALWAYS_INLINE__ EE_leds_off(void) 
+{
+	EE_led_0_off;
+	EE_led_1_off;
+}
+
+__INLINE__ void __ALWAYS_INLINE__ EE_leds_toggle(void) 
+{
+	EE_led_0_toggle;
+	EE_led_1_toggle;
+
 }
 
 
