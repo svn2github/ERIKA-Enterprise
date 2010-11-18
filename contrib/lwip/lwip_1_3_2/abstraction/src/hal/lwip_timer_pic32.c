829c44db376a627fd33a43e0652d411617247907
dario <dario@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-11-18 15:33:30 +0000

    lwip for pic32: adjusted timer period in accord with lwip documentation
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@891 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- contrib/lwip/lwip_1_3_2/abstraction/src/hal/lwip_timer_pic32.c
+++ contrib/lwip/lwip_1_3_2/abstraction/src/hal/lwip_timer_pic32.c
@@ -11,8 +11,7 @@
 void EE_hal_lwip_timers_configuration(void)
 {
 	/* Set the timer to be used with the lwip */
-	EE_timer_soft_init(EE_LWIP_TIMER_IN_USE,1000);	
-
+	EE_timer_soft_init(EE_LWIP_TIMER_IN_USE,EE_LWIP_TIMER_PERIOD_MS*1000);
 	EE_timer_set_callback(EE_LWIP_TIMER_IN_USE, EE_lwip_timer_tick);
 	EE_timer_start(EE_LWIP_TIMER_IN_USE); 
 }
