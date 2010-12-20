d67db86ea228b984a4fb5e5ac091f14884d5b1a2
dibbe <dibbe@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2010-12-20 16:03:53 +0000

    HV7131GP driver: removed unused boolean type definition  The definition may conflict with user code, and it is not used anyway
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@928 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- contrib/drivers/camera/hv7131gp/inc/hv7131gp_compiler_ee.h
+++ contrib/drivers/camera/hv7131gp/inc/hv7131gp_compiler_ee.h
@@ -11,27 +11,6 @@
 #define __hv7131gp_compiler_ee_h__
 
 #include <ee.h>
-//#include "p32xxxx.h"
-/** 
-* @name Basic type representations.
-* @{ */
-
-#if defined(__PIC32__)
-/* When supported, include plib.h to avoid conflicting definitions for boolean types */
-#include <plib.h>
-#else
-#ifndef DEF_BOOL_TYPE
-#define DEF_BOOL_TYPE
-/* chris: FIXME: do we really need this!? */
-typedef enum {
-	TRUE 		= 0x01,	/**< True */
-	FALSE		= 0x00, /**< False */
-} BOOL;
-typedef BOOL Boolean;
-#endif /* DEF_BOOL_TYPE */
-#endif
-
-/**  @} */
 
 #include "../../../../misc/inc/ee_stdint.h"
 
