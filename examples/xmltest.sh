e343bdae2700021618baffae50147720f02ac039
paolo <paolo@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2011-02-08 21:32:34 +0000

    regression tests on examples. quotes are now correct in the results.xml file
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@1000 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- examples/xmltest.sh
+++ examples/xmltest.sh
@@ -8,9 +8,9 @@
 
 if (test `grep "^$1\$" regression/current_ok_tests`); then
     # the test completed successfully
-    echo "  <test name=""$1"" executed=""yes"">"
+    echo "  <test name=\"$1\" executed=\"yes\">"
     echo "    <result>"
-    echo "      <success passed=""yes"" state=""100""/>"
+    echo "      <success passed=\"yes\" state=\"100\"/>"
     echo "    </result>"
     echo "    <description>"
     echo "      OK $1"
@@ -19,9 +19,9 @@ if (test `grep "^$1\$" regression/current_ok_tests`); then
     echo ""
 elif (test `grep "^$1\$" regression/current_witherrors_tests`); then
     # the test has an error
-    echo "  <test name=""$1"" executed=""yes"">"
+    echo "  <test name=\"$1\" executed=\"yes\">"
     echo "    <result>"
-    echo "      <success passed=""no"" state=""0""/>"
+    echo "      <success passed=\"no\" state=\"0\"/>"
     echo "    </result>"
     echo "    <description>"
     echo "      ERROR $1"
@@ -30,9 +30,9 @@ elif (test `grep "^$1\$" regression/current_witherrors_tests`); then
     echo ""
 elif (test `grep "^$1\$" regression/current_ignored_tests`); then
     # the test has been ignored
-    echo "  <test name=""$1"" executed=""no"">"
+    echo "  <test name=\"$1\" executed=\"no\">"
     echo "    <result>"
-    echo "      <success passed=""no"" state=""0""/>"
+    echo "      <success passed=\"no\" state=\"0\"/>"
     echo "    </result>"
     echo "    <description>"
     echo "      IGNORED $1"
@@ -43,9 +43,9 @@ else
     # the test is not listed (either in the template list or in 
     # the regression directories)
     # put an error!
-    echo "  <test name=""$1"" executed=""yes"">"
+    echo "  <test name=\"$1\" executed=\"yes\">"
     echo "    <result>"
-    echo "      <success passed=""no"" state=""0""/>"
+    echo "      <success passed=\"no\" state=\"0\"/>"
     echo "    </result>"
     echo "    <description>"
     echo "      ERRORNOTLISTED $1"
