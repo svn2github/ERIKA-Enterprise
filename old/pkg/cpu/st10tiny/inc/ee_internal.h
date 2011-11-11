d6feaeb5d14ddd88c4943aa3869c31a0e4bd04f5
outlawch <outlawch@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2011-11-11 15:44:55 +0000

    ST10TINY (old): Removes EE_hal_begin_primitive() and EE_hal_end_primitive() because are never called inside Erika kernels.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@1472 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- old/pkg/cpu/st10tiny/inc/ee_internal.h
+++ old/pkg/cpu/st10tiny/inc/ee_internal.h
@@ -54,18 +54,6 @@
  * Generic Primitives
  */
 
-/* called as first function of a generic primitive */
-__INLINE__ void __ALWAYS_INLINE__ EE_hal_begin_primitive(void)
-{
-  EE_hal_disableIRQ();
-}
-
-/* called as last function of a generic primitive */
-__INLINE__ void __ALWAYS_INLINE__ EE_hal_end_primitive(void)
-{
-  EE_hal_enableIRQ();
-}
-
 /* called as first function of a primitive called into an IRQ handler */
 __INLINE__ void __ALWAYS_INLINE__ EE_hal_IRQ_begin_primitive(void)
 {
