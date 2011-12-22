70012e6b56c54d63138c27d21461f8517fcf8fcc
outlawch <outlawch@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2011-12-22 10:47:48 +0000

    CORTEX-MX: Porting examples for TI Stellaris LM4F232xxxx with CCS toolchain and NXP LCPXPresso LPC12xx MCUs.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@1586 88d5ba13-baa4-4915-ae7a-41304cef7dd7

---
+++ examples/cortex_mx/lm4f232xxxx/porting_examples/monostack/EEtest05/code.c
@@ -0,0 +1,168 @@
+/* ###*B*###
+ * ERIKA Enterprise - a tiny RTOS for small microcontrollers
+ *
+ * Copyright (C) 2002-2011  Evidence Srl
+ *
+ * This file is part of ERIKA Enterprise.
+ *
+ * ERIKA Enterprise is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation,
+ * (with a special exception described below).
+ *
+ * Linking this code statically or dynamically with other modules is
+ * making a combined work based on this code.  Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this code with independent modules to produce an
+ * executable, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting executable under
+ * terms of your choice, provided that you also meet, for each linked
+ * independent module, the terms and conditions of the license of that
+ * module.  An independent module is a module which is not derived from
+ * or based on this library.  If you modify this code, you may extend
+ * this exception to your version of the code, but you are not
+ * obligated to do so.  If you do not wish to do so, delete this
+ * exception statement from your version.
+ *
+ * ERIKA Enterprise is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with ERIKA Enterprise; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ * ###*E*### */
+
+
+/*
+ * A minimal EE demo that demostrates how the nesting of the interrupts works.
+ * This demo can be used to test nesting mechanism.
+ * Author: 2011  Giuseppe Serano
+ * Based on examples/s12xs/porting_examples/monostack/EEtest5
+ */
+
+
+#include "ee.h"
+#include "cpu/cortex_mx/inc/ee_irq.h"
+#include "cpu/cortex_mx/inc/ee_svc.h"
+#include "test/assert/inc/ee_assert.h"
+
+#define TRUE 1
+
+/* Assertions */
+enum EE_ASSERTIONS {
+  EE_ASSERT_FIN = 0,
+  EE_ASSERT_INIT,
+  EE_ASSERT_TASK1_FIRED,
+  EE_ASSERT_SVC_ISR_FIRED,
+  EE_ASSERT_SYSTICK_ISR_FIRED,
+  EE_ASSERT_TASK1_ENDED,
+  EE_ASSERT_TASK2_FIRED,
+  EE_ASSERT_TASK2_ENDED,
+  EE_ASSERT_TASKS_ENDED,
+  EE_ASSERT_DIM
+};
+EE_TYPEASSERTVALUE EE_assertions[EE_ASSERT_DIM];
+
+/* Final result */
+EE_TYPEASSERTVALUE result;
+
+volatile int task1_fired = 0;
+volatile int task2_fired = 0;
+volatile int task1_ended = 0;
+volatile int task2_ended = 0;
+volatile int svcounter = 0;
+volatile int stcounter = 0;
+
+/*
+ * SVCall ISR2
+ */
+ISR2(svcall_handler)
+{
+  svcounter++;
+  EE_assert(EE_ASSERT_SVC_ISR_FIRED, svcounter == 1, EE_ASSERT_TASK1_FIRED);
+  while (stcounter < 10);
+  EE_systick_stop();
+}
+
+/*
+ * SysTick ISR2
+ */
+ISR2(systick_handler)
+{
+  stcounter++;
+  if (stcounter == 1)
+  {
+    ActivateTask(Task2);
+    EE_assert(
+      EE_ASSERT_SYSTICK_ISR_FIRED, stcounter == 1, EE_ASSERT_SVC_ISR_FIRED
+    );
+  }
+}
+
+/*
+ * TASK 1
+ */
+TASK(Task1)
+{
+  task1_fired++;
+  EE_assert(EE_ASSERT_TASK1_FIRED, task1_fired == 1, EE_ASSERT_INIT);
+  EE_systick_start();
+  EE_svc_0();
+  while (stcounter < 10);
+  task1_ended++;
+  EE_assert(
+    EE_ASSERT_TASK1_ENDED, task1_ended == 1, EE_ASSERT_SYSTICK_ISR_FIRED
+  );
+}
+
+/*
+ * TASK 2
+ */
+TASK(Task2)
+{
+  task2_fired++;
+  EE_assert(
+    EE_ASSERT_TASK2_FIRED, task2_fired == 1, EE_ASSERT_TASK1_ENDED);
+  task2_ended++;
+  EE_assert(EE_ASSERT_TASK2_ENDED, task2_ended == 1, EE_ASSERT_TASK2_FIRED);
+}
+
+/*
+ * MAIN TASK
+ */
+int main(void)
+{
+
+  /*Initializes Erika related stuffs*/
+  EE_system_init(); 
+
+  EE_user_led_init();
+
+  EE_systick_set_period(1000000);
+  EE_systick_enable_int();
+
+  EE_assert(EE_ASSERT_INIT, TRUE, EE_ASSERT_NIL);
+
+  ActivateTask(Task1);
+
+  EE_assert(
+    EE_ASSERT_TASKS_ENDED, task1_ended && task2_ended, EE_ASSERT_TASK2_ENDED
+  );
+  EE_assert_range(EE_ASSERT_FIN, EE_ASSERT_INIT, EE_ASSERT_TASKS_ENDED);
+  result = EE_assert_last();
+
+  /* Forever loop: background activities (if any) should go here */
+  for (;result;)
+  {
+    while (svcounter % 100000) svcounter++;
+    EE_user_led_toggle();
+    svcounter++;
+  }
+
+}
