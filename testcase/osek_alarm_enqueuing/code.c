9ac7aef4e295a5f34e1c54a9846405f229cab2fa
eguidieri <eguidieri@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2012-06-28 10:26:36 +0000

    Added another step in Alarm enqueuing test
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@2339 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- testcase/osek_alarm_enqueuing/code.c
+++ testcase/osek_alarm_enqueuing/code.c
@@ -56,7 +56,8 @@ enum EE_ASSERTION_TEST_1 {
   TEST_1_TICK3,
   TEST_1_ALARM1_CALLBACK2,
   TEST_1_TICK4,
-  TEST_1_END = TEST_1_TICK4,
+  TEST_1_BEFORE_END = TEST_1_ALARM1_CALLBACK2,
+  TEST_1_END        = TEST_1_TICK4,
   TEST_1_LENGTH
 };
 
@@ -72,11 +73,37 @@ enum EE_ASSERTION_TEST_2 {
   TEST_2_ALARM1_QUEUE,
   TEST_2_ALARM2_QUEUE,
   TEST_2_ALARM3_QUEUE,
-  TEST_2_END = TEST_2_ALARM3_QUEUE,
+  TEST_2_BEFORE_END = TEST_2_ALARM2_QUEUE,
+  TEST_2_END        = TEST_2_ALARM3_QUEUE,
   TEST_2_LENGTH
 };
 
-#define EE_ASSERTION_LENGTH TEST_2_LENGTH
+enum EE_ASSERTION_TEST_3 {
+  TEST_3_START = TEST_2_LENGTH,
+  TEST_3_ALARM1_CALLBACK1,
+  TEST_3_ALARM2_CALLBACK1,
+  TEST_3_ALARM3_CALLBACK1,
+  TEST_3_TICK1,
+  TEST_3_ALARM3_CALLBACK2,
+  TEST_3_TICK2,
+  TEST_3_ALARM2_CALLBACK2,
+  TEST_3_ALARM3_CALLBACK3,
+  TEST_3_TICK3,
+  TEST_3_ALARM1_CALLBACK2,
+  TEST_3_ALARM3_CALLBACK4,
+  TEST_3_TICK4,
+  TEST_3_ALARM2_QUEUE,
+  TEST_3_ALARM3_QUEUE,
+  TEST_3_ALARM1_QUEUE,
+  TEST_3_ALARM2_DELTA,
+  TEST_3_ALARM3_DELTA,
+  TEST_3_ALARM1_DELTA,
+  TEST_3_BEFORE_END = TEST_3_ALARM2_DELTA,
+  TEST_3_END        = TEST_3_ALARM1_DELTA,
+  TEST_3_LENGTH
+};
+
+#define EE_ASSERTION_LENGTH TEST_3_LENGTH
 
 /* assertion data */
 EE_TYPEASSERTVALUE EE_assertions[EE_ASSERTION_LENGTH];
@@ -112,7 +139,7 @@ void test1_alarm1_callback(void) {
       TEST_1_TICK3
     );
   } else {
-    EE_assert(TEST_1_END, FALSE, TEST_1_ALARM1_CALLBACK2);
+    EE_assert(TEST_1_END, FALSE, TEST_1_BEFORE_END);
   }
 
   ++test1_alarm1_callback_hits;
@@ -120,6 +147,8 @@ void test1_alarm1_callback(void) {
 
 static void test2(void) {
   StatusType s;
+  
+  /* Test set-up */
   SetRelAlarm(Test2Alarm1, 1, 1);
   SetRelAlarm(Test2Alarm2, 1, 1);
   s = SetRelAlarm(Test2Alarm3, 2, 2);
@@ -129,6 +158,7 @@ static void test2(void) {
   EE_assert(TEST_2_TICK1, (s == E_OK), TEST_2_START);
   s = IncrementCounter(Counter);
   EE_assert(TEST_2_TICK2, (s == E_OK), TEST_2_ALARM2_CALLBACK1);
+
   EE_assert(TEST_2_ALARM1_QUEUE,
     (EE_counter_RAM[Counter].first == Test2Alarm1),
     TEST_2_TICK2);
@@ -138,7 +168,8 @@ static void test2(void) {
   EE_assert(TEST_2_ALARM3_QUEUE,
     (EE_alarm_RAM[Test2Alarm2].next == Test2Alarm3),
     TEST_2_ALARM2_QUEUE);
-  
+
+  /* Test Clean-up */
   CancelAlarm(Test2Alarm1);
   CancelAlarm(Test2Alarm2);
   CancelAlarm(Test2Alarm3);
@@ -152,7 +183,7 @@ void test2_alarm1_callback(void) {
   } else if ( test2_alarm1_callback_hits == 1U ) {
     EE_assert(TEST_2_ALARM1_CALLBACK2, TRUE, TEST_2_ALARM3_CALLBACK1);
   } else {
-    EE_assert(TEST_2_END, FALSE, TEST_1_ALARM1_CALLBACK2);
+    EE_assert(TEST_2_END, FALSE, TEST_2_BEFORE_END);
   }
   ++test2_alarm1_callback_hits;
 }
@@ -164,7 +195,7 @@ void test2_alarm2_callback(void) {
   } else if ( test2_alarm2_callback_hits == 1U ) {
     EE_assert(TEST_2_ALARM2_CALLBACK2, TRUE, TEST_2_ALARM1_CALLBACK2);
   } else {
-    EE_assert(TEST_2_END, FALSE, TEST_1_ALARM1_CALLBACK2);
+    EE_assert(TEST_2_END, FALSE, TEST_2_BEFORE_END);
   }
   ++test2_alarm2_callback_hits;
 }
@@ -174,15 +205,101 @@ void test2_alarm3_callback(void) {
   if( test2_alarm3_callback_hits == 0U ) {
     EE_assert(TEST_2_ALARM3_CALLBACK1, TRUE, TEST_2_TICK1);
   } else {
-    EE_assert(TEST_2_END, FALSE, TEST_1_ALARM1_CALLBACK2);
+    EE_assert(TEST_2_END, FALSE, TEST_2_BEFORE_END);
   }
   ++test2_alarm3_callback_hits;
 }
+
+static void test3(void) {
+  StatusType s;
+  /* Test set-up */
+  SetRelAlarm(Test3Alarm1, 1, 3);
+  SetRelAlarm(Test3Alarm2, 1, 2);
+  s = SetRelAlarm(Test3Alarm3, 1, 1);
+
+  EE_assert(TEST_3_START, (s == E_OK), TEST_2_END);
+  s = IncrementCounter(Counter);
+  EE_assert(TEST_3_TICK1, (s == E_OK), TEST_3_ALARM3_CALLBACK1);
+  s = IncrementCounter(Counter);
+  EE_assert(TEST_3_TICK2, (s == E_OK), TEST_3_ALARM3_CALLBACK2);
+  s = IncrementCounter(Counter);
+  EE_assert(TEST_3_TICK3, (s == E_OK), TEST_3_ALARM3_CALLBACK3);
+  s = IncrementCounter(Counter);
+  EE_assert(TEST_3_TICK4, (s == E_OK), TEST_3_ALARM3_CALLBACK4);
+
+  EE_assert(TEST_3_ALARM2_QUEUE,
+    (EE_counter_RAM[Counter].first == Test3Alarm2),
+    TEST_3_TICK4);
+  EE_assert(TEST_3_ALARM3_QUEUE,
+    (EE_alarm_RAM[Test3Alarm2].next == Test3Alarm3),
+    TEST_3_ALARM2_QUEUE);
+  EE_assert(TEST_3_ALARM1_QUEUE,
+    (EE_alarm_RAM[Test3Alarm3].next == Test3Alarm1),
+    TEST_3_ALARM3_QUEUE);
+
+  EE_assert(TEST_3_ALARM2_DELTA,
+    (EE_alarm_RAM[Test3Alarm2].delta == 0U),
+    TEST_3_ALARM1_QUEUE);
+  EE_assert(TEST_3_ALARM3_DELTA,
+    (EE_alarm_RAM[Test3Alarm3].delta == 0U),
+    TEST_3_ALARM2_DELTA);
+  EE_assert(TEST_3_ALARM1_DELTA,
+    (EE_alarm_RAM[Test3Alarm1].delta == 2U),
+    TEST_3_ALARM3_DELTA);
+
+  /* Test Clean-up */
+  CancelAlarm(Test3Alarm1);
+  CancelAlarm(Test3Alarm2);
+  CancelAlarm(Test3Alarm3);
+}
+
+void test3_alarm1_callback(void) {
+  static EE_UREG test3_alarm1_callback_hits = 0U;
+
+  if( test3_alarm1_callback_hits == 0U ) {
+    EE_assert(TEST_3_ALARM1_CALLBACK1, TRUE, TEST_3_START);
+  } else if ( test3_alarm1_callback_hits == 1U ) {
+    EE_assert(TEST_3_ALARM1_CALLBACK2, TRUE, TEST_3_TICK3);
+  } else {
+    EE_assert(TEST_3_END, FALSE, TEST_3_BEFORE_END);
+  }
+  ++test3_alarm1_callback_hits;
+}
+
+void test3_alarm2_callback(void) {
+  static EE_UREG test3_alarm2_callback_hits = 0U;
+  if( test3_alarm2_callback_hits == 0U ) {
+    EE_assert(TEST_3_ALARM2_CALLBACK1, TRUE, TEST_3_ALARM1_CALLBACK1);
+  } else if ( test3_alarm2_callback_hits == 1U ) {
+    EE_assert(TEST_3_ALARM2_CALLBACK2, TRUE, TEST_3_TICK2);
+  } else {
+    EE_assert(TEST_3_END, FALSE, TEST_3_BEFORE_END);
+  }
+  ++test3_alarm2_callback_hits;
+}
+
+void test3_alarm3_callback(void) {
+  static EE_UREG test3_alarm3_callback_hits = 0U;
+  if( test3_alarm3_callback_hits == 0U ) {
+    EE_assert(TEST_3_ALARM3_CALLBACK1, TRUE, TEST_3_ALARM2_CALLBACK1);
+  } else if ( test3_alarm3_callback_hits == 1U ) {
+    EE_assert(TEST_3_ALARM3_CALLBACK2, TRUE, TEST_3_TICK1);
+  } else if ( test3_alarm3_callback_hits == 2U ) {
+    EE_assert(TEST_3_ALARM3_CALLBACK3, TRUE, TEST_3_ALARM2_CALLBACK2);
+  } else if ( test3_alarm3_callback_hits == 3U ) {
+    EE_assert(TEST_3_ALARM3_CALLBACK4, TRUE, TEST_3_ALARM1_CALLBACK2);
+  } else {
+    EE_assert(TEST_3_END, FALSE, TEST_3_BEFORE_END);
+  }
+  ++test3_alarm3_callback_hits;
+}
+
 
 TASK(TestTask)
 {
   test1();
   test2();
+  test3();
 
   TerminateTask();
 }
