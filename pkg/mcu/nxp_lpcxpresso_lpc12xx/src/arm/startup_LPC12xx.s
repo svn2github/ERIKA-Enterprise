4670b655893859d7f6e3da3046af87314b79f136
gfranchi <gfranchi@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2012-07-19 15:07:42 +0000

    Added the option  to call the SystemInit() function at startup before calling main(). This option is enabled by defining the  symbol __EE_DEF_SYS_INIT__. Note it can be defined by ASFLAGS in the oil file.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@2420 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- pkg/mcu/nxp_lpcxpresso_lpc12xx/src/arm/startup_LPC12xx.s
+++ pkg/mcu/nxp_lpcxpresso_lpc12xx/src/arm/startup_LPC12xx.s
@@ -117,8 +117,10 @@ Reset_Handler   PROC
                 EXPORT  Reset_Handler             [WEAK]
                 IMPORT  SystemInit
                 IMPORT  __main
-                LDR     R0, =SystemInit
+#ifdef __EE_DEF_SYS_INIT__				
+                LDR     R0, =SystemInit		
                 BLX     R0
+#endif						
                 LDR     R0, =__main
                 BX      R0
                 ENDP
