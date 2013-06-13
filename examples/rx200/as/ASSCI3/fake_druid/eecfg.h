cbd09d3dec28d0659149f5f7ec0a4d75170eecf4
gfranchi <gfranchi@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2013-06-13 16:45:12 +0000

    (AS, SCI, RX200) Fixed a bug on interrupt configuration in conf.oil of ASSCI3
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@2773 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- examples/rx200/as/ASSCI3/fake_druid/eecfg.h
+++ examples/rx200/as/ASSCI3/fake_druid/eecfg.h
@@ -1,55 +1,8 @@
-/* ###*B*###
- * ERIKA Enterprise - a tiny RTOS for small microcontrollers
- *
- * Copyright (C) 2002-2013  Evidence Srl
- *
- * This file is part of ERIKA Enterprise.
- *
- * ERIKA Enterprise is free software; you can redistribute it
- * and/or modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation, 
- * (with a special exception described below).
- *
- * Linking this code statically or dynamically with other modules is
- * making a combined work based on this code.  Thus, the terms and
- * conditions of the GNU General Public License cover the whole
- * combination.
- *
- * As a special exception, the copyright holders of this library give you
- * permission to link this code with independent modules to produce an
- * executable, regardless of the license terms of these independent
- * modules, and to copy and distribute the resulting executable under
- * terms of your choice, provided that you also meet, for each linked
- * independent module, the terms and conditions of the license of that
- * module.  An independent module is a module which is not derived from
- * or based on this library.  If you modify this code, you may extend
- * this exception to your version of the code, but you are not
- * obligated to do so.  If you do not wish to do so, delete this
- * exception statement from your version.
- *
- * ERIKA Enterprise is distributed in the hope that it will be
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License version 2 for more details.
- *
- * You should have received a copy of the GNU General Public License
- * version 2 along with ERIKA Enterprise; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA.
- * ###*E*### */
-
-/*
- * fake-druid eecfg.h
- *
- * Author: 2013  Gianluca Franchino
- */
-
-#ifndef __EECFG_H__
-#define __EECFG_H__
+#ifndef RTDH_EECFG_H
+#define RTDH_EECFG_H
 
 
-
-#define RTDRUID_CONFIGURATOR_NUMBER 1275
+#define RTDRUID_CONFIGURATOR_NUMBER 1277
 
 
 
@@ -62,9 +15,8 @@
     /* TASK definition */
     #define EE_MAX_TASK 3
     #define TaskTxOk 0
-	#define TaskRxOk 1
-	#define TaskRxErr 2
-
+    #define TaskRxOk 1
+    #define TaskRxErr 2
 
     /* MUTEX definition */
     #define EE_MAX_RESOURCE 0U
@@ -81,17 +33,12 @@
     /* CPUs */
     #define EE_MAX_CPU 1
     #define EE_CURRENTCPU 0
-	
-/***************************************************************************
- *
- * ISR
- *
- **************************************************************************/
-#define EE_RX200_TXI0_ISR EE_RX200_TXI0_ISR
-#define EE_RX200_ERI0_ISR EE_RX200_ERI0_ISR
-#define EE_RX200_RXI0_ISR EE_RX200_RXI0_ISR
+
+    /* Number of isr 2 */
+    #define EE_MAX_ISR2 3
 
 #ifndef __DISABLE_EEOPT_DEFINES__
+
 
 /***************************************************************************
  *
@@ -106,21 +53,34 @@
 #define __AS_SCI_DRIVER__
 #define __ADD_LIBS__
 
+
 /***************************************************************************
  *
  * Automatic options
  *
  **************************************************************************/
 #define __RTD_CYGWIN__
-#define __RX200__
 #define __R5F5210x__
+#define __RX200__
 #define __CCRX__
 #define __FP__
 #define __MONO__
 #define __FP_NO_RESOURCE__
 
-
 #endif
+
+
+
+/***************************************************************************
+ *
+ * ISR definition
+ *
+ **************************************************************************/
+#define EE_RX200_TXI0_ISR EE_RX200_TXI0_ISR
+#define EE_RX200_TXI0_ISR_PRI EE_ISR_PRI_1
+#define EE_RX200_ERI0_ISR EE_RX200_ERI0_ISR
+#define EE_RX200_RXI0_ISR EE_RX200_RXI0_ISR
+
 
 #endif
 
