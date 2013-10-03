6c771ac73a517ad34d9dc029821efd727fd0eafa
gfranchi <gfranchi@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2013-10-03 16:35:17 +0000

    STM32. Changed default value of  HSE clock to 8 MHz. Added the possibility to re-define PLL values.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@2852 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- pkg/mcu/st_stm32_stm32f4xx/src/system_stm32f4xx.c
+++ pkg/mcu/st_stm32_stm32f4xx/src/system_stm32f4xx.c
@@ -145,14 +145,24 @@
 
 
 /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N */
+#if !defined  (PLL_M)
 #define PLL_M      8
-#define PLL_N      336
+#endif
 
+#if !defined  (PLL_N)
+#define PLL_N      336
+#endif
+
+#if !defined  (PLL_P)
 /* SYSCLK = PLL_VCO / PLL_P */
 #define PLL_P      2
 
+#endif
+
+#if !defined  (PLL_Q)
 /* USB OTG FS, SDIO and RNG Clock =  PLL_VCO / PLLQ */
 #define PLL_Q      7
+#endif
 
 /**
   * @}
