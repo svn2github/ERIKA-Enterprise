b49ed7e7ff11d89d0f4ebbe412c4069eb8af7d77
gfranchi <gfranchi@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2014-05-09 16:46:43 +0000

    Modified conf.oil files in STM32F4xx porting examples to correctly include CMSIS and driver libraries for GCC compiler.
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@3031 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- examples/cortex_mx/STM32F4xx/porting_examples_GNU/monostack/EEtest02/fake_druid/eecfg.h
+++ examples/cortex_mx/STM32F4xx/porting_examples_GNU/monostack/EEtest02/fake_druid/eecfg.h
@@ -2,7 +2,7 @@
 #define RTDH_EECFG_H
 
 
-#define RTDRUID_CONFIGURATOR_NUMBER 1277
+#define RTDRUID_CONFIGURATOR_NUMBER 1278
 
 
 
@@ -20,10 +20,13 @@
     #define EE_MAX_RESOURCE 0U
 
     /* ALARM definition */
-    #define EE_MAX_ALARM 0
+    #define EE_MAX_ALARM 0U
+
+    /* COUNTER OBJECTS definition */
+    #define EE_MAX_COUNTER_OBJECTS EE_MAX_ALARM
 
     /* COUNTER definition */
-    #define EE_MAX_COUNTER 0
+    #define EE_MAX_COUNTER 0U
 
     /* APPMODE definition */
     #define EE_MAX_APPMODE 0U
@@ -31,6 +34,10 @@
     /* CPUs */
     #define EE_MAX_CPU 1
     #define EE_CURRENTCPU 0
+
+    /* Number of isr 2 */
+    #define EE_MAX_ISR2   1
+    #define EE_MAX_ISR_ID 1
 
 #ifndef __DISABLE_EEOPT_DEFINES__
 
@@ -53,13 +60,14 @@
  * Automatic options
  *
  **************************************************************************/
-#define __RTD_LINUX__
+#define __STM32__
+#define __STM32F4xx__
 #define __CORTEX_MX__
 #define __CORTEX_M4__
 #define __GNU__
 #define __FP__
 #define __MONO__
-#define __FP_NO_RESOURCES__
+#define __FP_NO_RESOURCE__
 
 #endif
 
@@ -71,7 +79,7 @@
  *
  **************************************************************************/
 #define EE_CORTEX_MX_SYSTICK_ISR systick_handler
-#define EE_CORTEX_MX_SYSTICK_ISR_PRI 1
+#define EE_CORTEX_MX_SYSTICK_ISR_PRI EE_ISR_PRI_1
 
 
 #endif
