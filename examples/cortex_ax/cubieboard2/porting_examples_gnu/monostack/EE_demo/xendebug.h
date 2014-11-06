beb567a26c404a46b6573a2c3ca233b692812f87
ava <ava@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2014-11-06 23:36:28 +0000

    erika, xen, debug: add safety checks on strlen() function
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@3201 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- examples/cortex_ax/cubieboard2/porting_examples_gnu/monostack/EE_demo/xendebug.h
+++ examples/cortex_ax/cubieboard2/porting_examples_gnu/monostack/EE_demo/xendebug.h
@@ -2,16 +2,24 @@
 #define _INCLUDES_DEBUG_ARM_32_H_
 
 #define MAX_MSGLEN	1024
+#define STRERR		"EE: console error\n"
+#define STRERR_LEN	18
 
 extern void HYPERVISOR_console_io(int what, int len, char *msg);
 
-int strln(const char *s)
+int strln(char *s)
 {
     int i = 0;
+
+    if (s == NULL)
+	return -1;
+    /* Safety: last character is always a \n */
+    s[MAX_MSGLEN-1] = '\n';
     while (s[i] != '\n' && i < MAX_MSGLEN) i++;
     return ++i;
 }
 
-#define printk(s)	HYPERVISOR_console_io(CONSOLEIO_write, strln(s), s);
+#define printk(s)	HYPERVISOR_console_io(CONSOLEIO_write, strln(s), s)
+#define panic_printk()	HYPERVISOR_console_io(CONSOLEIO_write, STRERR_LEN, STRERR)
 
 #endif /* _INCLUDES_DEBUG_ARM_32_H_ */
