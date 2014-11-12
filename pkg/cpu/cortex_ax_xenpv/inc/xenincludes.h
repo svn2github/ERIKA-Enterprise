712767dd0830238d1bffb5dff58058b5921f1fb4
bmorelli <bmorelli@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2014-11-12 10:07:27 +0000

    cortex_ax_xenpv, refactor of xenincludes
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@3231 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- pkg/cpu/cortex_ax_xenpv/inc/xenincludes.h
+++ pkg/cpu/cortex_ax_xenpv/inc/xenincludes.h
@@ -1,3 +1,47 @@
+/* ###*B*###
+ * ERIKA Enterprise - a tiny RTOS for small microcontrollers
+ *
+ * Copyright (C) 2002-2013  Evidence Srl
+ *
+ * This file is part of ERIKA Enterprise.
+ *
+ * ERIKA Enterprise is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation,
+ * (with a special exception described below).
+ *
+ * Linking this code statically or dynamically with other modules is
+ * making a combined work based on this code.  Thus, the terms and
+ * conditions of the GNU General Public License cover the whole
+ * combination.
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this code with independent modules to produce an
+ * executable, regardless of the license terms of these independent
+ * modules, and to copy and distribute the resulting executable under
+ * terms of your choice, provided that you also meet, for each linked
+ * independent module, the terms and conditions of the license of that
+ * module.  An independent module is a module which is not derived from
+ * or based on this library.  If you modify this code, you may extend
+ * this exception to your version of the code, but you are not
+ * obligated to do so.  If you do not wish to do so, delete this
+ * exception statement from your version.
+ *
+ * ERIKA Enterprise is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License version 2 for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * version 2 along with ERIKA Enterprise; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA.
+ * ###*E*### */
+
+/*
+ * Author: 2014 Arianna Avanzini
+ */
+
 #ifndef _INCLUDES_ARM_32_H_
 #define _INCLUDES_ARM_32_H_
 
@@ -341,8 +385,6 @@ typedef struct _ev_action_t {
     uint32_t count;
 } ev_action_t;
 #define NR_EVS 1024
-static ev_action_t ev_actions[NR_EVS];
-static unsigned long bound_ports[NR_EVS/(8*sizeof(unsigned long))];
 
 extern shared_info_t shared_info;
 extern shared_info_t *HYPERVISOR_shared_info;
@@ -353,64 +395,9 @@ static inline void clear_evtchn(uint32_t port)
     synch_clear_bit(port, &s->evtchn_pending[0]);
 }
 
-int do_event(evtchn_port_t port, struct pt_regs *regs)
-{
-    ev_action_t  *action;
-
-    clear_evtchn(port);
-
-    if ( port >= NR_EVS ) {
-        printk("EE: ERROR: do_event\n");
-        return 1;
-    }
-
-    action = &ev_actions[port];
-    action->count++;
-
-    /* call the handler */
-        action->handler(port, regs, action->data);
-
-    return 1;
-
-}
-
+int do_event(evtchn_port_t port, struct pt_regs *regs);
 evtchn_port_t bind_evtchn(evtchn_port_t port, evtchn_handler_t handler,
-                                                  void *data)
-{
-        ev_actions[port].data = data;
-        wmb();
-        ev_actions[port].handler = handler;
-        set_bit(port, bound_ports);
-
-        return port;
-}
-
-void timer_handler(evtchn_port_t port, struct pt_regs *regs, void *ign)
-{
-        printk("timer kick\n");
-	/*get_time_values_from_xen();
-        update_wallclock();*/
-}
-
-void increment_vtimer_compare(uint64_t inc) {
-        uint32_t x, y;
-        uint64_t value;
-        __asm__ __volatile__("mrrc p15, 1, %0, %1, c14\n"
-                        "isb":"=r"(x), "=r"(y));
-
-        // CompareValue = Counter + VTIMER_TICK
-        value = (0xFFFFFFFFFFFFFFFFULL & x) | ((0xFFFFFFFFFFFFFFFFULL & y) << 32);
-        value += inc;
-        x = 0xFFFFFFFFULL & value;
-        y = (value >> 32) & 0xFFFFFFFF;
-
-        __asm__ __volatile__("mcrr p15, 3, %0, %1, c14\n"
-                        "isb"::"r"(x), "r"(y));
-
-        __asm__ __volatile__("mov %0, #0x1\n"
-                                "mcr p15, 0, %0, c14, c3, 1\n" /* Enable timer and unmask the output signal */
-                                "isb":"=r"(x));
-}
+                                                  void *data);
 
 #define L1_PAGETABLE_SHIFT     	   12
 #define PFN_PHYS(x)   		   ((uint64_t)(x) << L1_PAGETABLE_SHIFT)
