2d0953d2700935289133638e7765794ab57615ba
bmorelli <bmorelli@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2014-11-12 10:07:22 +0000

    cortex_ax_xenpv, refactor of xendebug
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@3230 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- pkg/cpu/cortex_ax_xenpv/src/xendebug.c
+++ pkg/cpu/cortex_ax_xenpv/src/xendebug.c
@@ -42,20 +42,62 @@
  * Author: 2014 Arianna Avanzini
  */
 
-#ifndef _INCLUDES_DEBUG_ARM_32_H_
-#define _INCLUDES_DEBUG_ARM_32_H_
+#include "cpu/cortex_ax_xenpv/inc/xendebug.h"
+#include "cpu/cortex_ax_xenpv/inc/ee_cax_cpu.h"
 
-#define MAX_MSGLEN	1024
-#define STRERR		"EE: console error\n"
-#define STRERR_LEN	18
+int cut_string(char *buffer, int length)
+{
+	int i = 0;
 
-extern void HYPERVISOR_console_io(int what, int len, char *msg);
+	for (i = 0 ; i < length ; i++)
+		if (buffer[i] == 0) {
+			buffer[i] = '\n';
+			break;
+		}
 
-char *itoa(int i, char *b);
-void print_number(int n);
-int strln(char *s);
+	return i+1;
+}
 
-#define printk(s)	HYPERVISOR_console_io(0, strln(s), s)
-#define panic_printk()	HYPERVISOR_console_io(0, STRERR_LEN, STRERR)
+char *itoa(int i, char *b)
+{
+	char const digit[] = "0123456789";
+	char *p = b;
+	int shifter;
+	if (i < 0) {
+		*p++ = '-';
+		i *= -1;
+	}
+	shifter = i;
+	do {
+		++p;
+		shifter = shifter / 10;
+	} while(shifter);
+	*p = '\0';
+	do {
+		*--p = digit[i%10];
+		i = i / 10;
+	} while(i);
+	return b;
+}
 
-#endif /* _INCLUDES_DEBUG_ARM_32_H_ */
+int strln(char *s)
+{
+    int i = 0;
+
+    if (s == NULL)
+	return -1;
+    /* Safety: last character is always a \n */
+    s[MAX_MSGLEN-1] = '\n';
+    while (s[i] != '\n' && s[i] != '\0' && i < MAX_MSGLEN) i++;
+    if (s[i] == '\0')
+        s[i] = '\n';
+    return ++i;
+}
+
+void print_number(int n)
+{
+	char buf[20];
+
+	itoa(n, buf);
+	printk(buf);
+}
