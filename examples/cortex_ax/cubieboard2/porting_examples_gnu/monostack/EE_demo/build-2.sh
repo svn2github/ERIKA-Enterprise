b12aaf52d01ae2a912c9bbb91aa26a7cae11b242
bmorelli <bmorelli@88d5ba13-baa4-4915-ae7a-41304cef7dd7>
2014-11-12 10:08:16 +0000

    erika, cleanup of build and clean scripts
    git-svn-id: svn://svn.tuxfamily.org/svnroot/erika/erikae/repos/ee/trunk/ee@3241 88d5ba13-baa4-4915-ae7a-41304cef7dd7

--- examples/cortex_ax/cubieboard2/porting_examples_gnu/monostack/EE_demo/build-2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export ECLIPSE_HOME=/home/bruno/usr/ee2/eclipse
-export RTDRUID_ECLIPSE_HOME=/home/bruno/gea_kern/xen/rt_tux_web_CLEAN/
-export CORTEX_AX_XENPV_GCCDIR=/usr/
-
-#make FAKE_DRUID=1 $@
-make $@
